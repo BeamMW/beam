2367b16d4911eea90ceeb21bc1f732eb375dd36b
Vadim Grigoruk <vadim@beam-mw.com>
2018-03-28 13:30:17 +0300

    miner: Travis config WIP

--- miner/pow/travis-install-libsodium.sh
+++ miner/pow/travis-install-libsodium.sh
@@ -10,7 +10,7 @@ if [ ! -d "$HOME/libsodium/lib" ]; then
   wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
   tar xvfz libsodium-1.0.16.tar.gz
   cd libsodium-1.0.16
-  ./configure --prefix=$HOME/libsodium
+  ./configure
   make
   sudo make install
 else
