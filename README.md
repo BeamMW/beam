bdbaa0a0f33f3f962ac0a20ead1586aec30221be
Vadim Grigoruk <vadim@beam-mw.com>
2018-03-28 12:30:06 +0300

    miner: Travis config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.com/beam-mw/beam.svg?token=9GsAxqTGpjsBMMHVvgzz&branch=miner)](https://travis-ci.com/beam-mw/beam)
+
 # beam
---
+++ miner/pow/travis-install-libsodium.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# The purpose of this file is to install libsodium in
+# the Travis CI environment. Outside this environment,
+# you would probably not want to install it like this.
+
+set -e
+
+# check if libsodium is already installed
+if [ ! -d "$HOME/libsodium/lib" ]; then
+  wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+  tar xvfz libsodium-1.0.16.tar.gz
+  cd libsodium-1.0.16
+  ./configure --prefix=$HOME/libsodium
+  make
+  make install
+else
+  echo 'Using cached directory.'
+fi
