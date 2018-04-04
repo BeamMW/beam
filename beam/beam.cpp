2636bc19f70e066c0c6f36aa203c388af70ffe0c
Vadim Grigoruk <vadim@beam-mw.com>
2018-04-04 15:26:49 +0300

    wallet: added boost programm option lib

--- beam/beam.cpp
+++ beam/beam.cpp
@@ -1,6 +1,39 @@
 #include "core/common.h"
 
-int main(int argc, char* argvp[])
+#include <iostream>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+
+int main(int argc, char* argv[])
 {
+    po::options_description desc("Allowed options");
+
+    desc.add_options()
+        ("help", "produce help message")
+        ("compression", po::value<int>(), "set compression level")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) 
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    if (vm.count("compression")) 
+    {
+        std::cout << "Compression level was set to " 
+            << vm["compression"].as<int>() << ".\n";
+    } 
+    else 
+    {
+        std::cout << "Compression level was not set.\n";
+    }
+
     return 0;
 }
