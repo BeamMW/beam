7b13f3a19f20f0792898c8a59dcdedf6708fd2b6
vladik <valdo@beam-mw.com>
2018-05-31 02:50:44 +0300

    misc fixes

--- beam/beam.cpp
+++ beam/beam.cpp
@@ -223,7 +223,11 @@ int main(int argc, char* argv[])
                 }
                 node.m_Cfg.m_WalletKey = walletSeed;
 
-				std::vector<std::string> vPeers = vm[cli::NODE_PEER].as<std::vector<std::string> >();
+				std::vector<std::string> vPeers;
+
+				if (vm.count(cli::NODE_PEER))
+					vPeers = vm[cli::NODE_PEER].as<std::vector<std::string> >();
+
 				node.m_Cfg.m_Connect.resize(vPeers.size());
 
 				for (size_t i = 0; i < vPeers.size(); i++)
@@ -398,7 +402,7 @@ int main(int argc, char* argv[])
 						// Leave this until mining
 
 						SerializerFile ser;
-						ser.m_File.open(sPath, std::ofstream::out | std::ofstream::trunc);
+						ser.m_File.open(sPath, std::ofstream::out | std::ofstream::trunc | std::ofstream::binary);
 						ser & block;
 
 					}
