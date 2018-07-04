010b5f0dd7166ed2b54f8970656bb6a8e31067ee
roman.strilets <roman@beam-mw.com>
2018-07-04 17:29:32 +0300

    functional-tests: send wrong NewTip message

--- beam/functionaltests/send_wrong_newtip_test.cpp
+++ beam/functionaltests/send_wrong_newtip_test.cpp
@@ -17,12 +17,18 @@ private:
 	virtual void OnConnected() override;
 	virtual bool OnMsg2(proto::Boolean&& msg) override;
 	virtual bool OnMsg2(proto::NewTip&& msg) override;	
+	virtual bool OnMsg2(proto::GetHdr&& msg) override;
+	virtual bool OnMsg2(proto::GetBody&& msg) override;
 
 	void GenerateTests() override;
+
+private:
+	Height m_Height;
 };
 
 TestNodeConnection::TestNodeConnection(int argc, char* argv[])
 	: BaseTestNodeConnection(argc, argv)
+	, m_Height(0)
 {
 }
 
@@ -39,13 +45,54 @@ bool TestNodeConnection::OnMsg2(proto::Boolean&& msg)
 bool TestNodeConnection::OnMsg2(proto::NewTip&& msg)
 {
 	LOG_INFO() << "NewTip: " << msg.m_ID;
-	msg.m_ID.m_Height += 3;
 
-	proto::NewTip newMsg;
+	if (m_Height == 0)
+	{
+		LOG_INFO() << "Send NewTip with height = " << msg.m_ID.m_Height + 5;
+		proto::NewTip newMsg;
 
-	newMsg.m_ID = msg.m_ID;
-	
-	Send(newMsg);
+		newMsg.m_ID = msg.m_ID;
+		newMsg.m_ID.m_Height += 5;
+		m_Height = newMsg.m_ID.m_Height;
+
+		Send(newMsg);
+
+		m_Timer->start(3 * 60 * 1000, false, [this]()
+		{
+			m_Failed = true;
+			io::Reactor::get_Current().stop();
+		});
+	}
+	else
+	{
+		if (msg.m_ID.m_Height == m_Height)
+		{
+			LOG_INFO() << "Ok";
+		}
+		else
+		{
+			LOG_INFO() << "Failed";
+		}
+		io::Reactor::get_Current().stop();
+	}
+
+	return true;
+}
+
+bool TestNodeConnection::OnMsg2(proto::GetHdr&& msg)
+{
+	LOG_INFO() << "GetHdr: " << msg.m_ID.m_Height;
+
+	proto::Hdr msgHdr;
+	Send(msgHdr);
+	return true;
+}
+bool TestNodeConnection::OnMsg2(proto::GetBody&& msg)
+{
+	LOG_INFO() << "GetHdr: " << msg.m_ID.m_Height;
+
+	proto::Body msgHdr;
+	Send(msgHdr);
 	return true;
 }
 
@@ -63,7 +110,6 @@ int main(int argc, char* argv[])
 
 	TestNodeConnection connection(argc, argv);
 
-	connection.DisabledTimer();
 	connection.Run();
 
 	return connection.CheckOnFailed();
