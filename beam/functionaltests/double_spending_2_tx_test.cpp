a0df52fe59f180fa31034161d6ec51aa4f703644
roman.strilets <strylets@gmail.com>
2018-07-18 09:36:59 +0300

    functional-tests: double spending - 2 transactions

--- beam/functionaltests/double_spending_2_tx_test.cpp
+++ beam/functionaltests/double_spending_2_tx_test.cpp
@@ -19,7 +19,8 @@ private:
 	bool m_IsInit;
 	bool m_IsNeedToCheckOut;
 	unsigned int m_Counter;
-	TxGenerator m_Generator;
+	TxGenerator m_Generator1;
+	TxGenerator m_Generator2;
 	CoinsChecker m_CoinsChecker;
 };
 
@@ -28,7 +29,8 @@ TestNodeConnection::TestNodeConnection(int argc, char* argv[])
 	, m_IsInit(false)
 	, m_IsNeedToCheckOut(false)
 	, m_Counter(0)
-	, m_Generator(m_Kdf)
+	, m_Generator1(m_Kdf)
+	, m_Generator2(m_Kdf)
 	, m_CoinsChecker(argc, argv)
 {
 	m_Timeout = 5 * 60 * 1000;
@@ -50,19 +52,40 @@ void TestNodeConnection::OnMsg(proto::NewTip&& msg)
 	{
 		m_IsInit = true;
 
-		m_Generator.GenerateInputInTx(msg.m_ID.m_Height - 70, Rules::get().CoinbaseEmission);
-		m_Generator.GenerateInputInTx(msg.m_ID.m_Height - 70, Rules::get().CoinbaseEmission);
-		m_Generator.GenerateOutputInTx(msg.m_ID.m_Height + 1, Rules::get().CoinbaseEmission);
-		m_Generator.GenerateKernel(msg.m_ID.m_Height + 5, Rules::get().CoinbaseEmission);
-		m_Generator.Sort();
-		Send(m_Generator.GetTransaction());
+		m_Generator1.GenerateInputInTx(msg.m_ID.m_Height - 70, Rules::get().CoinbaseEmission);
+		m_Generator1.GenerateOutputInTx(msg.m_ID.m_Height + 1, Rules::get().CoinbaseEmission);
+		m_Generator1.GenerateKernel(msg.m_ID.m_Height + 5);
+		m_Generator1.Sort();
+		Send(m_Generator1.GetTransaction());
+
+		m_Generator2.GenerateInputInTx(msg.m_ID.m_Height - 70, Rules::get().CoinbaseEmission);
+		m_Generator2.GenerateOutputInTx(msg.m_ID.m_Height + 2, Rules::get().CoinbaseEmission);
+		m_Generator2.GenerateKernel(msg.m_ID.m_Height + 6);
+		m_Generator2.Sort();
+		Send(m_Generator2.GetTransaction());
 	}
 
 	if (m_IsNeedToCheckOut)
 	{
-		if (++m_Counter >= 2)
+		if (++m_Counter >= 3)
 		{
-			m_CoinsChecker.Check(m_Generator.GenerateInputsFromOutputs(),
+			m_CoinsChecker.Check(m_Generator1.GenerateInputsFromOutputs(),
+				[this](bool isOk)
+				{
+					if (isOk)
+					{
+						LOG_INFO() << "Ok: utxo is valid";
+					}
+					else
+					{
+						LOG_INFO() << "Failed: utxo is not valid";
+						m_Failed = true;
+					}
+					//io::Reactor::get_Current().stop();
+				}
+			);
+
+			m_CoinsChecker.Check(m_Generator2.GenerateInputsFromOutputs(),
 				[this](bool isOk)
 				{
 					if (isOk)
