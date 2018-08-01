101602950d046932d15f7fa52803b7bc0cd3da40
Vadim Grigoruk <vadim@beam-mw.com>
2018-08-01 12:50:34 +0300

    ci: added Latest folder to beam-builds repo

--- deploy.sh
+++ deploy.sh
@@ -16,20 +16,25 @@ RELEASE_FOLDER=$DATE_FOLDER/release
 
 if [[ "$OSTYPE" == "linux"* ]]; then
     OS_FOLDER=$RELEASE_FOLDER/linux
+    LATEST_OS_FOLDER=latest/linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     OS_FOLDER=$RELEASE_FOLDER/mac
+    LATEST_OS_FOLDER=latest/mac
 else
     echo "Error, OS:$OSTYPE not supported"
     exit 1
 fi
 
 mkdir -p $OS_FOLDER
+mkdir -p $LATEST_OS_FOLDER
 
 tar -cvf $OS_FOLDER/$APP.tar --directory=$HOME/build/beam-mw/beam/beam beam beam.cfg
 tar -rvf $OS_FOLDER/$APP.tar --directory=$HOME/build/beam-mw/beam/ui beam-ui beam-ui.cfg
 tar -czvf $OS_FOLDER/$APP.tar.gz --directory=$OS_FOLDER $APP.tar
 
-git add $OS_FOLDER/$APP.tar.gz
+cp -f $OS_FOLDER/$APP.tar.gz $LATEST_OS_FOLDER/$APP.tar.gz
+
+git add $OS_FOLDER/$APP.tar.gz $LATEST_OS_FOLDER/$APP.tar.gz
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER on $OSTYPE"
 git push $REPO master
 
