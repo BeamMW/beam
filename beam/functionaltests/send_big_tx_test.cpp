71a7c6b75bc3d7efffad92e3468bec2a780de0ef
Anatol <anatol@beam-mw.com>
2018-09-10 13:10:28 +0300

    restored lost changes

--- beam/functionaltests/send_big_tx_test.cpp
+++ beam/functionaltests/send_big_tx_test.cpp
@@ -29,7 +29,7 @@ class TestNodeConnection : public BaseTestNode
 public:
 	TestNodeConnection(int argc, char* argv[]);
 private:
-
+	
 	void OnDisconnect(const DisconnectReason& ) override;
 	void GenerateTests() override;
 };
@@ -50,7 +50,7 @@ void TestNodeConnection::GenerateTests()
 {
 	m_Tests.push_back([this]()
 	{
-		LOG_INFO() << "Send big transaction";
+		LOG_INFO() << "Generate transaction";
 		TxGenerator gen(m_Kdf);
 
 		Amount amount = 20000;
@@ -61,7 +61,7 @@ void TestNodeConnection::GenerateTests()
 		// Outputs
 		for (Amount i = 0; i < amount; ++i)
 		{
-			gen.GenerateOutputInTx(1, 1);
+			gen.GenerateOutputInTx(1, 1, beam::KeyType::Regular, true);
 		}
 
 		// Kernels
@@ -69,6 +69,7 @@ void TestNodeConnection::GenerateTests()
 
 		gen.Sort();
 
+        LOG_INFO() << "Send big transaction";
         Send(gen.GetTransaction());
 	});
 }
@@ -85,5 +86,5 @@ int main(int argc, char* argv[])
 
 	connection.Run();
 
-	return connection.CheckOnFailed();
+	return connection.CheckOnFailed();	
 }
