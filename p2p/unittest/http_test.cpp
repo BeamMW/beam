4b7202217ac334db31d8e1d2cdaaa2329d82bd41
Artem <artem@beam-mw.com>
2018-09-26 16:26:23 +0300

    p2p: test for http communicaton #1

--- p2p/unittest/http_test.cpp
+++ p2p/unittest/http_test.cpp
@@ -16,6 +16,7 @@
 #include "p2p/http_msg_creator.h"
 #include "utility/io/tcpserver.h"
 #include "utility/io/asyncevent.h"
+#include "utility/io/timer.h"
 #include "utility/helpers.h"
 #include "utility/logger.h"
 
@@ -24,6 +25,7 @@ using namespace beam;
 namespace {
 
 const uint16_t PORT = 8765;
+std::string THIS_PATH;
 io::AsyncEvent::Trigger g_stopEvent;
 
 class DummyHttpServer {
@@ -102,6 +104,7 @@ private:
         }
 
         _theConnection.reset();
+        _serialized.clear();
         if (stop) {
             g_stopEvent();
         }
@@ -115,15 +118,118 @@ private:
     HttpConnection::Ptr _theConnection;
 };
 
+class DummyHttpClient {
+public:
+    DummyHttpClient(io::Reactor& reactor) :
+        _msgCreator(1000),
+        _reactor(reactor),
+        _timer(io::Timer::create(_reactor))
+    {
+        _timer->start(100, false, BIND_THIS_MEMFN(on_timer));
+    }
+
+    int uncompleted = 1;
+
+private:
+    void on_timer() {
+        if (!_reactor.tcp_connect(io::Address::localhost().port(PORT), 333, BIND_THIS_MEMFN(on_connected), 1000)) {
+            LOG_ERROR() << "Connect failed";
+            g_stopEvent();
+        }
+    }
+
+    void on_connected(uint64_t, io::TcpStream::Ptr&& newStream, io::ErrorCode errorCode) {
+        if (errorCode != 0) {
+            LOG_ERROR() << "Connect failed, " << io::error_str(errorCode);
+            g_stopEvent();
+            return;
+        }
+
+        _theConnection = std::make_unique<HttpConnection>(
+            222,
+            BaseConnection::outbound,
+            BIND_THIS_MEMFN(on_response),
+            10*1024*1024,
+            1024,
+            std::move(newStream)
+        );
+
+        static const HeaderPair headers[] = {
+            {"Host", "zzz.xxx" }
+        };
+
+        if (_msgCreator.create_request(
+            _serialized,
+            "GET",
+            THIS_PATH.c_str(),
+            headers, 1
+        )) {
+            _theConnection->write_msg(_serialized);
+            _serialized.clear();
+        } else {
+            LOG_ERROR() << "Cannot send request";
+            g_stopEvent();
+        }
+    }
+
+    bool on_response(uint64_t, const HttpMsgReader::Message& msg) {
+        if (msg.what != HttpMsgReader::http_message || !msg.msg) {
+            if (msg.what == HttpMsgReader::connection_error && msg.connectionError == io::EC_EOF) {
+                _theConnection.reset();
+            }
+            else {
+                g_stopEvent();
+            }
+            return false;
+        }
+
+        size_t bodySize=0;
+        const void* body = msg.msg->get_body(bodySize);
+        if (bodySize) {
+            try {
+                io::SharedBuffer fileContent = io::map_file_read_only(THIS_PATH.c_str());
+                if (fileContent.size == bodySize && !memcmp(body, fileContent.data, bodySize)) {
+                    uncompleted = 0;
+                    LOG_INFO() << "file received successfully, size=" << bodySize;
+                } else {
+                    LOG_INFO() << "file receive error" << TRACE(bodySize) << TRACE(fileContent.size);
+                }
+            } catch (const std::exception& e) {
+                LOG_ERROR() << e.what();
+            }
+        }
+
+        _theConnection.reset();
+        g_stopEvent();
+        return false;
+    }
+
+    HttpMsgCreator _msgCreator;
+    io::SerializedMsg _serialized;
+    io::Reactor& _reactor;
+    HttpConnection::Ptr _theConnection;
+    io::Timer::Ptr _timer;
+};
+
+void get_this_path() {
+    char buf[4096];
+    size_t sz=4096;
+    uv_exepath(buf, &sz);
+    THIS_PATH = std::string(buf);
+}
+
 int http_server_test() {
     int nErrors = 0;
 
     try {
+        get_this_path();
         io::Reactor::Ptr reactor = io::Reactor::create();
         io::AsyncEvent::Ptr stopEvent = io::AsyncEvent::create(*reactor, [&reactor]() {reactor->stop();});
         g_stopEvent = stopEvent;
         DummyHttpServer server(*reactor);
+        DummyHttpClient client(*reactor);
         reactor->run();
+        nErrors = client.uncompleted;
     } catch (const std::exception& e) {
         LOG_ERROR() << e.what();
         nErrors = 255;
@@ -135,5 +241,10 @@ int http_server_test() {
 } //namespace
 
 int main() {
-    http_server_test();
+    int logLevel = LOG_LEVEL_DEBUG;
+#if LOG_VERBOSE_ENABLED
+    logLevel = LOG_LEVEL_VERBOSE;
+#endif
+    auto logger = Logger::create(logLevel, logLevel);
+    return http_server_test();
 }
