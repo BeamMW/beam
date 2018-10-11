810bfdfa8d9ac162693ef5bdff38ac9186eb0151
roman.strilets <strylets@gmail.com>
2018-10-11 16:39:56 +0300

    atomic_swap: added example of getblockchaininfor request to bitcoin node

--- libbitcoin/examples/get_info.cpp
+++ libbitcoin/examples/get_info.cpp
@@ -12,134 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "p2p/http_connection.h"
-#include "p2p/http_msg_creator.h"
-#include "utility/io/tcpserver.h"
-#include "utility/io/asyncevent.h"
+#include "p2p/http_client.h"
 #include "utility/io/timer.h"
 #include "utility/helpers.h"
 #include "utility/logger.h"
 
 #include "bitcoin/bitcoin.hpp"
 
+#include "nlohmann/json.hpp"
+
 #include <iostream>
 
 using namespace beam;
+using json = nlohmann::json;
 
 namespace 
 {
-io::AsyncEvent::Trigger g_stopEvent;
 const uint16_t PORT = 18443;
-
-class DummyHttpClient {
-public:
-    DummyHttpClient(io::Reactor& reactor) :
-        _msgCreator(1000),
-        _reactor(reactor),
-        _timer(io::Timer::create(_reactor))
-    {
-        _timer->start(100, false, BIND_THIS_MEMFN(on_timer));
-    }
-
-    int uncompleted = 1;
-
-private:
-    void on_timer() {
-        if (!_reactor.tcp_connect(io::Address::localhost().port(PORT), 333, BIND_THIS_MEMFN(on_connected), 1000)) {
-            LOG_ERROR() << "Connect failed";
-            g_stopEvent();
-        }
-    }
-
-    void on_connected(uint64_t, io::TcpStream::Ptr&& newStream, io::ErrorCode errorCode) {
-        if (errorCode != 0) {
-            LOG_ERROR() << "Connect failed, " << io::error_str(errorCode);
-            g_stopEvent();
-            return;
-        }
-
-        _theConnection = std::make_unique<HttpConnection>(
-            222,
-            BaseConnection::outbound,
-            BIND_THIS_MEMFN(on_response),
-            10 * 1024 * 1024,
-            1024,
-            std::move(newStream)
-            );
-
-        std::string userWithPass("test:123");
-        libbitcoin::data_chunk t(userWithPass.begin(), userWithPass.end());
-
-        LOG_INFO() << std::string("Basic ") + libbitcoin::encode_base64(t);
-        //"test:123"
-        static const HeaderPair headers[] = {
-            {"Host", "127.0.0.1" },
-            {"Connection", "close"},
-            {"Authorization", (std::string("Basic ") + libbitcoin::encode_base64(t)).c_str()}
-        };
-
-        const char *data = "{\"method\":\"getblockchaininfo\",\"params\":[],\"id\":1}\n";
-            //"{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getblockchaininfo\", \"params\": [] }";
-
-        if (_msgCreator.create_request(
-            _serialized,
-            "POST",
-            "/",
-            headers, 3, 1,
-            data, strlen(data)
-        )) {
-            _theConnection->write_msg(_serialized);
-            _serialized.clear();
-        }
-        else {
-            LOG_ERROR() << "Cannot send request";
-            g_stopEvent();
-        }
-    }
-
-    bool on_response(uint64_t, const HttpMsgReader::Message& msg) {
-        LOG_INFO() << "on_response";
-        if (msg.what != HttpMsgReader::http_message || !msg.msg) {
-            LOG_INFO() << "not http: " << msg.what;
-            if (msg.what == HttpMsgReader::connection_error && msg.connectionError == io::EC_EOF) {
-                _theConnection.reset();
-            }
-            else {
-                g_stopEvent();
-            }
-            return false;
-        }
-
-        size_t bodySize = 0;
-        const void* body = msg.msg->get_body(bodySize);
-        if (bodySize) {
-            std::cout << body << std::endl;
-            /*try {
-                io::SharedBuffer fileContent = io::map_file_read_only(THIS_PATH.c_str());
-                if (fileContent.size == bodySize && !memcmp(body, fileContent.data, bodySize)) {
-                    uncompleted = 0;
-                    LOG_INFO() << "file received successfully, size=" << bodySize;
-                }
-                else {
-                    LOG_INFO() << "file receive error" << TRACE(bodySize) << TRACE(fileContent.size);
-                }
-            }
-            catch (const std::exception& e) {
-                LOG_ERROR() << e.what();
-            }*/
-        }
-
-        _theConnection.reset();
-        g_stopEvent();
-        return false;
-    }
-
-    HttpMsgCreator _msgCreator;
-    io::SerializedMsg _serialized;
-    io::Reactor& _reactor;
-    HttpConnection::Ptr _theConnection;
-    io::Timer::Ptr _timer;
-};
 }
 
 int main() {
@@ -151,9 +40,46 @@ int main() {
 
     try {
         io::Reactor::Ptr reactor = io::Reactor::create();
-        io::AsyncEvent::Ptr stopEvent = io::AsyncEvent::create(*reactor, [&reactor]() {reactor->stop(); });
-        g_stopEvent = stopEvent;
-        DummyHttpClient client(*reactor);
+        HttpClient client(*reactor);
+
+        {
+            std::string userWithPass("test:123");
+            libbitcoin::data_chunk t(userWithPass.begin(), userWithPass.end());
+            std::string auth("Basic " + libbitcoin::encode_base64(t));
+            std::string_view authView(auth);
+            static const HeaderPair headers[] = {
+                {"Host", "127.0.0.1" },
+                {"Connection", "close"},
+                {"Authorization", authView.data()}
+            };
+            const char *data = "{\"method\":\"getblockchaininfo\",\"params\":[],\"id\":1}\n";
+
+            HttpClient::Request request;
+            io::Address a(io::Address::localhost(), PORT);
+
+            request.address(a);
+            request.connectTimeoutMsec(2000);
+            request.pathAndQuery("/");
+            request.headers(headers);
+            request.numHeaders(3);
+            request.method("POST");
+            request.body(data, strlen(data));
+
+            request.callback([&reactor](uint64_t id, const HttpMsgReader::Message& msg) -> bool {
+                LOG_INFO() << "response from " << id;
+                size_t sz = 0;
+                const void* body = msg.msg->get_body(sz);
+                json j = json::parse(std::string(static_cast<const char*>(body), sz));
+
+                LOG_INFO() << j;
+                LOG_INFO() << j["result"]["headers"];
+                reactor->stop();
+                return false;
+            });
+
+            client.send_request(request);
+        }
+
         reactor->run();
     }
     catch (const std::exception& e) {
