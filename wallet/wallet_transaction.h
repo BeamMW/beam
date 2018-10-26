f5e3ee4935fdc5feb0727dce7bf948ec046bf208
Vadim Grigoruk <vadim@beam-mw.com>
2018-10-26 16:22:49 +0300

    transaction fail fix

--- wallet/wallet_transaction.h
+++ wallet/wallet_transaction.h
@@ -140,7 +140,7 @@ namespace beam { namespace wallet
         
         // deduced values, 
         TxKernel::Ptr m_Kernel;
-        ECC::Scalar::Native m_PeerSignature;
+        ECC::Scalar m_PeerSignature;
         ECC::Hash::Value m_Message;
         ECC::Signature::MultiSig m_MultiSig;
 
