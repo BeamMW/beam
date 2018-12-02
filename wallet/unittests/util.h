267f6f3642e1ed0fb20214c8dd9230f1af1f3e3c
valdok <valdo@beam-mw.com>
2018-12-02 21:19:29 +0200

    Deleted outdated treasury generator

--- wallet/unittests/util.h
+++ wallet/unittests/util.h
@@ -6,7 +6,6 @@ namespace beam {
 IWalletDB::Ptr init_wallet_db(const std::string& path, ECC::uintBig* walletSeed);
 
 bool ReadTreasury(std::vector<Block::Body>& vBlocks, const std::string& sPath);
-int GenerateTreasury(IWalletDB*, const std::string& sPath, uint32_t nCount, Height dh, Amount v);
 
 } //namespace
 
