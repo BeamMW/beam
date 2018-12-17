ff1820474a1882510208fb7ba94049bc75d09ec8
Anatol <anatol@beam-mw.com>
2018-12-17 14:46:41 +0300

    #138 fixed indecies compression. tests.

--- 3rdparty/crypto/equihash.h
+++ 3rdparty/crypto/equihash.h
@@ -161,6 +161,11 @@ inline constexpr size_t equihash_solution_size(unsigned int N, unsigned int K) {
     return (1 << K)*(N/(K+1)+1)/8;
 }
 
+constexpr uint8_t GetSizeInBytes(size_t N)
+{
+    return static_cast<uint8_t>((N + 7) / 8);
+}
+
 template<unsigned int N, unsigned int K>
 class Equihash
 {
@@ -170,7 +175,7 @@ private:
 
 public:
     enum : size_t { IndicesPerHashOutput=512/N };
-    enum : size_t { HashOutput = 512/8 };
+    enum : size_t { HashOutput = IndicesPerHashOutput * GetSizeInBytes(N) };
     enum : size_t { CollisionBitLength=N/(K+1) };
     enum : size_t { CollisionByteLength=(CollisionBitLength+7)/8 };
     enum : size_t { HashLength=(K+1)*CollisionByteLength };
--- pow/unittests/equihash_test.cpp
+++ pow/unittests/equihash_test.cpp
@@ -14,34 +14,113 @@
 
 #include "core/block_crypt.h"
 #include <iostream>
+#include "3rdparty/crypto/equihash.h"
+#include "wallet/unittests/test_helpers.h"
+#include <algorithm>
+
+WALLET_TEST_INIT
+using namespace std;
+
+void TestArrayExpanding(size_t N, size_t K)
+{
+    cout << "Test array expanding: " << N << "," << K << "...\n";
+    size_t bitsLeft = N;
+    size_t collisionBits = N / (K + 1);
+    size_t collisionBytes = (collisionBits + 7) / 8;
+    size_t outBytes = sizeof(uint32_t);
+    size_t bytePad = outBytes - (collisionBits + 7) / 8;
+    size_t outputSize = (K + 1) * (collisionBytes + bytePad);
+    size_t inputSize = (N + 7) / 8;
+
+    vector<uint8_t> input(inputSize, 0);
+    for (size_t i = 0; i < inputSize - 1; ++i)
+    {
+        input[i] = 0xc0 + uint8_t(i);//0xff;
+        bitsLeft -= 8;
+    }
+    WALLET_CHECK(bitsLeft <= 8);
+    input[inputSize - 1] = 0xff << (8 - bitsLeft);
+    vector<uint8_t> output(outputSize, 0);
+    ExpandArray(input.data(), input.size(), output.data(), output.size(), collisionBits, bytePad);
+
+    for (size_t i = outBytes; i < output.size(); i += outBytes)
+    {
+   //     WALLET_CHECK(equal(&output[i], &output[i] + outBytes, &output[0]));
+    }
+
+    vector<uint8_t> temp(input.size(), 0);
+    CompressArray(output.data(), output.size(), &temp[0], input.size(), collisionBits, bytePad);
+    WALLET_CHECK(equal(temp.begin(), temp.end(), input.begin()));
+}
+
+void TestArrayExpanding()
+{
+    {
+        vector<uint8_t> output(8, 0);
+        vector<uint8_t> temp(7, 0);
+        vector<uint8_t> input = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfc };
+        ExpandArray(input.data(), input.size(), output.data(), output.size(), 27, 0);
+        CompressArray(output.data(), output.size(), &temp[0], temp.size(), 27, 0);
+        WALLET_CHECK(temp[6] == 0xfc);
+    }
+    {
+        vector<uint8_t> output( 8, 0 );
+        vector<uint8_t> input = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0 };
+        ExpandArray(input.data(), input.size(), output.data(), output.size(), 26, 0);
+    }
+    {
+        vector<uint8_t> output = {0x3, 0xff, 0xff, 0xff, 0x3, 0xff, 0xff, 0xff };
+        vector<uint8_t> temp(7, 0);
+        CompressArray(output.data(), output.size(), &temp[0], temp.size(), 26, 0);
+        WALLET_CHECK(temp[6] == 0xf0);
+    }
+    {
+        vector<uint8_t> output(8, 0);
+        vector<uint8_t> temp(7, 0);
+        vector<uint8_t> input = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfc };
+        ExpandArray(input.data(), input.size(), output.data(), output.size(), 27, 0);
+        CompressArray(output.data(), output.size(), &temp[0], temp.size(), 27, 0);
+        WALLET_CHECK(temp[6] == 0xfc);
+    }
+    TestArrayExpanding(156, 5);
+    TestArrayExpanding(120, 5);
+    TestArrayExpanding(144, 5);
+    TestArrayExpanding(150, 5);
+    TestArrayExpanding(96, 5);
+}
 
 int main()
 {
-    uint8_t pInput[] = {1, 2, 3, 4, 56};
-
-	beam::Block::PoW pow;
-	pow.m_Difficulty = 0; // d=0, runtime ~48 sec. d=1,2 - almost close to this. d=4 - runtime 4 miuntes, several cycles until solution is achieved.
-	pow.m_Nonce = 0x010204U;
-
-    // our builder doesn't support GPU
-//#if defined (BEAM_USE_GPU)
-//    {
-//	    pow.SolveGPU(pInput, sizeof(pInput));
-//
-//        if (!pow.IsValid(pInput, sizeof(pInput)))
-//		    return -1;
-//    }
-//#else
-
+    TestArrayExpanding();
+    
     {
-        pow.Solve(pInput, sizeof(pInput));
+        cout << "Test PoW...\n";
+        uint8_t pInput[] = { 1, 2, 3, 4, 56 };
 
-        if (!pow.IsValid(pInput, sizeof(pInput)))
-            return -1;
+        beam::Block::PoW pow;
+        pow.m_Difficulty = 0; // d=0, runtime ~48 sec. d=1,2 - almost close to this. d=4 - runtime 4 miuntes, several cycles until solution is achieved.
+        pow.m_Nonce = 0x010204U;
+
+        // our builder doesn't support GPU
+    //#if defined (BEAM_USE_GPU)
+    //    {
+    //	    pow.SolveGPU(pInput, sizeof(pInput));
+    //
+    //        if (!pow.IsValid(pInput, sizeof(pInput)))
+    //		    return -1;
+    //    }
+    //#else
+
+        {
+            pow.Solve(pInput, sizeof(pInput));
+
+            WALLET_CHECK(pow.IsValid(pInput, sizeof(pInput)));
+        }
+
+        //#endif
+
+        std::cout << "Solution is correct\n";
     }
-
-//#endif
-
-    std::cout << "Solution is correct\n";
-    return 0;
+    assert(g_failureCount == 0);
+    return WALLET_CHECK_RESULT;
 }
