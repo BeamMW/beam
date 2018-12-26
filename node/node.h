f89f279fca4419b791d7bc7519eb08be0884ab01
valdok <valdo@beam-mw.com>
2018-12-26 16:09:43 +0200

    BBS msg liftime reduced to 12 hours

--- node/node.h
+++ node/node.h
@@ -59,7 +59,7 @@ struct Node
 			uint32_t m_TopPeersUpd_ms = 1000 * 60 * 10; // once in 10 minutes
 			uint32_t m_PeersUpdate_ms	= 1000; // reconsider every second
 			uint32_t m_PeersDbFlush_ms = 1000 * 60; // 1 minute
-			uint32_t m_BbsMessageTimeout_s	= 3600 * 24; // 1 day
+			uint32_t m_BbsMessageTimeout_s	= 3600 * 12; // 1/2 day
 			uint32_t m_BbsMessageMaxAhead_s	= 3600 * 2; // 2 hours
 			uint32_t m_BbsCleanupPeriod_ms = 3600 * 1000; // 1 hour
 		} m_Timeout;
