6a7995b5237e6b81b087a515349ff30fa051ca35
valdok <valdo@beam-mw.com>
2019-01-01 20:37:28 +0200

    NodeProcessor - logging treasury bursts

--- node/processor.cpp
+++ node/processor.cpp
@@ -538,10 +538,26 @@ bool NodeProcessor::HandleTreasury(const Blob& blob, bool bFirstTime)
 		return false;
 	}
 
-	if (bFirstTime && !td.IsValid())
+	if (bFirstTime)
 	{
-		LOG_WARNING() << "Treasury validation failed";
-		return false;
+		if (!td.IsValid())
+		{
+			LOG_WARNING() << "Treasury validation failed";
+			return false;
+		}
+
+		std::vector<Treasury::Data::Burst> vBursts = td.get_Bursts();
+
+		std::ostringstream os;
+		os << "Treasury check. Total bursts=" << vBursts.size();
+
+		for (size_t i = 0; i < vBursts.size(); i++)
+		{
+			const Treasury::Data::Burst& b = vBursts[i];
+			os << "\n\t" << "Height=" << b.m_Height << ", Value=" << b.m_Value;
+		}
+
+		LOG_INFO() << os.str();
 	}
 
 	for (size_t iG = 0; iG < td.m_vGroups.size(); iG++)
