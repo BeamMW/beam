e2c867358ba01c998acc932114b922b9a73339b2
Vadim Grigoruk <vadim@beam-mw.com>
2019-03-07 20:02:53 +0300

    #495 added block searching by a kernel id to node explorer api

--- explorer/adapter.h
+++ explorer/adapter.h
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "utility/io/buffer.h"
+#include "utility/common.h"
 
 namespace beam {
 
@@ -30,6 +31,8 @@ struct IAdapter {
     virtual bool get_status(io::SerializedMsg& out) = 0;
 
     virtual bool get_block(io::SerializedMsg& out, uint64_t height) = 0;
+
+    virtual bool get_block(io::SerializedMsg& out, const ByteBuffer& key) = 0;
 
     virtual bool get_blocks(io::SerializedMsg& out, uint64_t startHeight, uint64_t n) = 0;
 };
