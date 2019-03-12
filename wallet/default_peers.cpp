bbc933adb641c51726645151d5bcae75a55d5c91
roman.strilets <strylets@gmail.com>
2019-03-12 13:24:19 +0300

    #90 gave list of default peers for android
    # Conflicts:
    #	android/node_model.cpp
    #	node/processor.h

---
+++ wallet/default_peers.cpp
@@ -0,0 +1,42 @@
+// Copyright 2019 The Beam Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+
+#include "default_peers.h"
+
+namespace beam
+{
+    std::vector<std::string> getDefaultPeers()
+    {
+        std::vector<std::string> result
+        {
+#ifdef BEAM_TESTNET
+            "ap-node01.testnet.beam.mw:8100",
+            "ap-node02.testnet.beam.mw:8100",
+            "ap-node03.testnet.beam.mw:8100",
+            "eu-node01.testnet.beam.mw:8100",
+            "eu-node02.testnet.beam.mw:8100",
+            "eu-node03.testnet.beam.mw:8100",
+            "us-node01.testnet.beam.mw:8100",
+            "us-node02.testnet.beam.mw:8100",
+            "us-node03.testnet.beam.mw:8100"
+#else
+            "eu-node01.masternet.beam.mw:8100",
+            "eu-node02.masternet.beam.mw:8100",
+            "eu-node03.masternet.beam.mw:8100",
+            "eu-node04.masternet.beam.mw:8100"
+#endif
+        };
+
+        return result;
+    }
+}
