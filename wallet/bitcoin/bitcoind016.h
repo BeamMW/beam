c578e27b34f39c40ab5d6625324d5e1f64ce19c5
Roman Strilets <roman@beam-mw.com>
2019-06-14 15:48:22 +0300

    #717 output bitcoin/litecoin error to log

--- wallet/bitcoin/bitcoind016.h
+++ wallet/bitcoin/bitcoind016.h
@@ -18,6 +18,11 @@
 #include "http/http_client.h"
 #include "options.h"
 
+//namespace nlohmann
+//{
+//    class json;
+//}
+
 namespace beam
 {
     class Bitcoind016: public IBitcoinBridge
@@ -48,7 +53,7 @@ namespace beam
         uint32_t getLockTimeInBlocks() const override;
 
     protected:
-        void sendRequest(const std::string& method, const std::string& params, std::function<void(const Error&, const std::string&)> callback);
+        void sendRequest(const std::string& method, const std::string& params, std::function<void(const Error&, const nlohmann::json&)> callback);
         bool isMainnet() const;
 
     private:
