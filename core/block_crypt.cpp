8eeac5b7941fc8a8720161b673f76b012b35e70d
vladislav <vgelfer@m-booster.com>
2019-07-10 14:16:10 +0300

    Rules: added sanity check for fork heights

--- core/block_crypt.cpp
+++ core/block_crypt.cpp
@@ -1046,8 +1046,23 @@ namespace beam
 		}
 	}
 
+	bool Rules::IsForkHeightsConsistent() const
+	{
+		if (pForks[0].m_Height != Rules::HeightGenesis - 1)
+			return false;
+
+		for (size_t i = 1; i < _countof(pForks); i++)
+			if (pForks[i].m_Height < pForks[i - 1].m_Height)
+				return false;
+
+		return true;
+	}
+
 	void Rules::UpdateChecksum()
 	{
+		if (!IsForkHeightsConsistent())
+			throw std::runtime_error("Inconsistent Forks");
+
 		// all parameters, including const (in case they'll be hardcoded to different values in later versions)
 		ECC::Oracle oracle;
 		oracle
