855489d3cbcdaba68746df8fe81673f30d69280c
valdok <valdo@beam-mw.com>
2019-08-22 17:17:36 +0300

    MappedFile moved into separate .h/cpp files

--- core/navigator.h
+++ core/navigator.h
@@ -14,75 +14,10 @@
 
 #pragma once
 #include "block_crypt.h"
+#include "mapped_file.h"
 
 namespace beam
 {
-	class MappedFile
-	{
-	public:
-		typedef uint64_t Offset;
-
-	private:
-		struct Bank {
-			Offset m_Tail;
-		};
-
-		static uint32_t s_PageSize;
-
-#ifdef WIN32
-		HANDLE m_hFile;
-		HANDLE m_hMapping;
-#else // WIN32
-		int m_hFile;
-#endif // WIN32
-
-		Offset m_nMapping;
-		uint8_t* m_pMapping;
-		uint32_t m_nBank0;
-		uint32_t m_nBanks;
-
-		void ResetVarsFile();
-		void ResetVarsMapping();
-		void CloseMapping();
-		void OpenMapping();
-		//void Write(const void*, uint32_t);
-		//void WriteZero(uint32_t);
-		void Resize(Offset);
-		Bank& get_Bank(uint32_t iBank);
-
-	public:
-
-		MappedFile();
-		~MappedFile();
-
-		struct Defs
-		{
-			const uint8_t* m_pSig;
-			uint32_t m_nSizeSig;
-			uint32_t m_nBanks;
-			uint32_t m_nFixedHdr;
-
-			uint32_t get_Bank0() const;
-			uint32_t get_SizeMin() const;
-		};
-
-		void Open(const char* sz, const Defs&);
-		void Close();
-
-		void* get_FixedHdr() const;
-
-		template <typename T> T& get_At(Offset n) const
-		{
-			assert(m_pMapping && (m_nMapping >= n + sizeof(T)));
-			return *(T*) (m_pMapping  + n);
-		}
-
-		Offset get_Offset(const void* p) const;
-
-		void* Allocate(uint32_t iBank, uint32_t nSize);
-		void Free(uint32_t iBank, void*);
-	};
-
 	class ChainNavigator
 	{
 	protected:
