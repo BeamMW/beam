0b7e7bff9926a57321c405597c1987dcf7ad4973
Anatol <anatol@beam-mw.com>
2019-11-05 23:15:39 +0300

    #1061 AbstractDataModel in utxo view

--- ui/viewmodel/utxo/utxo_view.cpp
+++ ui/viewmodel/utxo/utxo_view.cpp
@@ -13,119 +13,27 @@
 // limitations under the License.
 
 #include "utxo_view.h"
-#include "ui_helpers.h"
+#include "viewmodel/ui_helpers.h"
 #include "model/app_model.h"
 using namespace beam;
 using namespace beam::wallet;
 using namespace std;
 using namespace beamui;
 
-namespace
-{
-template<typename T>
-bool compareUtxo(const T& lf, const T& rt, Qt::SortOrder sortOrder)
-{
-    if (sortOrder == Qt::DescendingOrder)
-        return lf > rt;
-    return lf < rt;
-}
-}
-
-UtxoItem::UtxoItem(const beam::wallet::Coin& coin)
-    : _coin{ coin }
-{
-
-}
-
-UtxoItem::~UtxoItem()
-{
-
-}
-
-QString UtxoItem::amount() const
-{
-    return AmountToUIString(_coin.m_ID.m_Value, Currencies::Beam);
-}
-
-QString UtxoItem::maturity() const
-{
-    if (!_coin.IsMaturityValid())
-        return QString{ "-" };
-    return QString::number(_coin.m_maturity);
-}
-
-UtxoViewStatus::EnStatus UtxoItem::status() const
-{
-    switch(_coin.m_status)
-    {
-        case Coin::Available:
-            return UtxoViewStatus::Available;
-        case Coin::Maturing:
-            return UtxoViewStatus::Maturing;
-        case Coin::Unavailable:
-            return UtxoViewStatus::Unavailable;
-        case Coin::Outgoing:
-            return UtxoViewStatus::Outgoing;
-        case Coin::Incoming:
-			return UtxoViewStatus::Incoming;
-        case Coin::Spent:
-            return UtxoViewStatus::Spent;
-        default:
-            assert(false && "Unknown key type");
-    }
-
-    return UtxoViewStatus::Undefined;
-}
-
-UtxoViewType::EnType UtxoItem::type() const
-{
-    switch (_coin.m_ID.m_Type)
-    {
-        case Key::Type::Comission: return UtxoViewType::Comission;
-        case Key::Type::Coinbase: return UtxoViewType::Coinbase;
-        case Key::Type::Regular: return UtxoViewType::Regular;
-        case Key::Type::Change: return UtxoViewType::Change;
-        case Key::Type::Treasury: return UtxoViewType::Treasury;
-    }
-
-    return UtxoViewType::Undefined;
-}
-
-beam::Amount UtxoItem::rawAmount() const
-{
-    return _coin.m_ID.m_Value;
-}
-
-const beam::wallet::Coin::ID& UtxoItem::get_ID() const
-{
-	return _coin.m_ID;
-}
-
-beam::Height UtxoItem::rawMaturity() const
-{
-    return _coin.get_Maturity();
-}
-
-
 UtxoViewModel::UtxoViewModel()
     : _model{*AppModel::getInstance().getWallet()}
-    , _sortOrder(Qt::DescendingOrder)
 {
-    connect(&_model, SIGNAL(allUtxoChanged(const std::vector<beam::wallet::Coin>&)),
-        SLOT(onAllUtxoChanged(const std::vector<beam::wallet::Coin>&)));
+    connect(&_model, SIGNAL(allUtxoChanged(beam::wallet::ChangeAction, const std::vector<beam::wallet::Coin>&)),
+        SLOT(onAllUtxoChanged(beam::wallet::ChangeAction, const std::vector<beam::wallet::Coin>&)));
     connect(&_model, SIGNAL(stateIDChanged()), SIGNAL(stateChanged()));
 
     _model.getAsync()->getUtxosStatus();
 }
 
-UtxoViewModel::~UtxoViewModel()
-{
-    qDeleteAll(_allUtxos);
-}
 
-QQmlListProperty<UtxoItem> UtxoViewModel::getAllUtxos()
+QAbstractItemModel* UtxoViewModel::getAllUtxos()
 {
-    return QQmlListProperty<UtxoItem>(this, _allUtxos);
+    return & _allUtxos;
 }
 
 QString UtxoViewModel::getCurrentHeight() const
@@ -138,109 +46,46 @@ QString UtxoViewModel::getCurrentStateHash() const
     return QString(beam::to_hex(_model.getCurrentStateID().m_Hash.m_pData, 10).c_str());
 }
 
-QString UtxoViewModel::sortRole() const
+void UtxoViewModel::onAllUtxoChanged(beam::wallet::ChangeAction action, const std::vector<beam::wallet::Coin>& utxos)
 {
-    return _sortRole;
-}
+    vector<shared_ptr<UtxoItem>> modifiedItems;
+    modifiedItems.reserve(utxos.size());
 
-void UtxoViewModel::setSortRole(const QString& value)
-{
-    _sortRole = value;
-    sortUtxos();
-}
-
-Qt::SortOrder UtxoViewModel::sortOrder() const
-{
-    return _sortOrder;
-}
-
-void UtxoViewModel::setSortOrder(Qt::SortOrder value)
-{
-    _sortOrder = value;
-    sortUtxos();
-}
-
-void UtxoViewModel::onAllUtxoChanged(const std::vector<beam::wallet::Coin>& utxos)
-{
-    // TODO: It's dirty hack. Should use QAbstractListModel instead of QQmlListProperty
-    auto tmpList = _allUtxos;
-    
-    _allUtxos.clear();
-
-    emit allUtxoChanged();
-
-    qDeleteAll(tmpList);
-
-    for (const auto& utxo : utxos)
+    for (const auto& t : utxos)
     {
-        _allUtxos.push_back(new UtxoItem(utxo));
+        modifiedItems.push_back(make_shared<UtxoItem>(t));
     }
 
-    sortUtxos();
-}
+    switch (action)
+    {
+    case ChangeAction::Reset:
+    {
+        _allUtxos.reset(modifiedItems);
+        break;
+    }
 
-void UtxoViewModel::sortUtxos()
-{
-    auto cmp = generateComparer();
-    std::sort(_allUtxos.begin(), _allUtxos.end(), cmp);
+    case ChangeAction::Removed:
+    {
+        _allUtxos.remove(modifiedItems);
+        break;
+    }
+
+    case ChangeAction::Added:
+    {
+        _allUtxos.insert(modifiedItems);
+        break;
+    }
+
+    case ChangeAction::Updated:
+    {
+        _allUtxos.update(modifiedItems);
+        break;
+    }
+
+    default:
+        assert(false && "Unexpected action");
+        break;
+    }
 
     emit allUtxoChanged();
-}
-
-QString UtxoViewModel::amountRole() const
-{
-    return "amount";
-}
-
-QString UtxoViewModel::heightRole() const
-{
-    return "height";
-}
-
-QString UtxoViewModel::maturityRole() const
-{
-    return "maturity";
-}
-
-QString UtxoViewModel::statusRole() const
-{
-    return "status";
-}
-
-QString UtxoViewModel::typeRole() const
-{
-    return "type";
-}
-
-std::function<bool(const UtxoItem*, const UtxoItem*)> UtxoViewModel::generateComparer()
-{
-    if (_sortRole == amountRole())
-        return [sortOrder = _sortOrder](const UtxoItem* lf, const UtxoItem* rt)
-    {
-        return compareUtxo(lf->rawAmount(), rt->rawAmount(), sortOrder);
-    };
-
-    if (_sortRole == maturityRole())
-        return [sortOrder = _sortOrder](const UtxoItem* lf, const UtxoItem* rt)
-    {
-        return compareUtxo(lf->rawMaturity(), rt->rawMaturity(), sortOrder);
-    };
-
-    if (_sortRole == statusRole())
-        return [sortOrder = _sortOrder](const UtxoItem* lf, const UtxoItem* rt)
-    {
-        return compareUtxo(lf->status(), rt->status(), sortOrder);
-    };
-
-    if (_sortRole == typeRole())
-        return [sortOrder = _sortOrder](const UtxoItem* lf, const UtxoItem* rt)
-    {
-        return compareUtxo(lf->type(), rt->type(), sortOrder);
-    };
-
-    // defult
-    return [sortOrder = _sortOrder](const UtxoItem* lf, const UtxoItem* rt)
-    {
-        return compareUtxo(lf->get_ID(), rt->get_ID(), sortOrder);
-    };
 }
--- ui/viewmodel/utxo/utxo_view.h
+++ ui/viewmodel/utxo/utxo_view.h
@@ -15,86 +15,28 @@
 #pragma once
 
 #include <QObject>
-#include <QQmlListProperty>
 #include "model/wallet_model.h"
-#include "viewmodel/utxo_view_status.h"
-#include "viewmodel/utxo_view_type.h"
-
-class UtxoItem : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(QString amount       READ amount     NOTIFY changed)
-    Q_PROPERTY(QString maturity     READ maturity   NOTIFY changed)
-    Q_PROPERTY(int status           READ status     NOTIFY changed)
-    Q_PROPERTY(int type             READ type       NOTIFY changed)
-public:
-
-    UtxoItem() = default;
-    UtxoItem(const beam::wallet::Coin& coin);
-    virtual ~UtxoItem();
-
-    QString amount() const;
-    QString maturity() const;
-    UtxoViewStatus::EnStatus status() const;
-    UtxoViewType::EnType type() const;
-
-    beam::Amount rawAmount() const;
-    beam::Height rawMaturity() const;
-	const beam::wallet::Coin::ID& get_ID() const;
-
-signals:
-    void changed();
-
-private:
-    beam::wallet::Coin _coin;
-};
+#include "utxo_item_list.h"
 
 class UtxoViewModel : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY(QQmlListProperty<UtxoItem> allUtxos READ getAllUtxos NOTIFY allUtxoChanged)
-    Q_PROPERTY(QString currentHeight READ getCurrentHeight NOTIFY stateChanged)
-    Q_PROPERTY(QString currentStateHash READ getCurrentStateHash NOTIFY stateChanged)
-
-    Q_PROPERTY(QString sortRole READ sortRole WRITE setSortRole)
-    Q_PROPERTY(Qt::SortOrder sortOrder READ sortOrder WRITE setSortOrder)
-
-    Q_PROPERTY(QString amountRole READ amountRole CONSTANT)
-    Q_PROPERTY(QString heightRole READ heightRole CONSTANT)
-    Q_PROPERTY(QString maturityRole READ maturityRole CONSTANT)
-    Q_PROPERTY(QString statusRole READ statusRole CONSTANT)
-    Q_PROPERTY(QString typeRole READ typeRole CONSTANT)
+    Q_PROPERTY(QAbstractItemModel* allUtxos  READ getAllUtxos          NOTIFY allUtxoChanged)
+    Q_PROPERTY(QString currentHeight         READ getCurrentHeight     NOTIFY stateChanged)
+    Q_PROPERTY(QString currentStateHash      READ getCurrentStateHash  NOTIFY stateChanged)
 
 public:
     UtxoViewModel();
-    virtual ~UtxoViewModel();
-    QQmlListProperty<UtxoItem> getAllUtxos();
+    QAbstractItemModel* getAllUtxos();
     QString getCurrentHeight() const;
     QString getCurrentStateHash() const;
-    QString sortRole() const;
-    void setSortRole(const QString&);
-    Qt::SortOrder sortOrder() const;
-    void setSortOrder(Qt::SortOrder);
 public slots:
-    void onAllUtxoChanged(const std::vector<beam::wallet::Coin>& utxos);
+    void onAllUtxoChanged(beam::wallet::ChangeAction, const std::vector<beam::wallet::Coin>& utxos);
 signals:
     void allUtxoChanged();
     void stateChanged();
 private:
-
-    void sortUtxos();
-
-    QString amountRole() const;
-    QString heightRole() const;
-    QString maturityRole() const;
-    QString statusRole() const;
-    QString typeRole() const;
-
-    std::function<bool(const UtxoItem*, const UtxoItem*)> generateComparer();
-
-    QList<UtxoItem*> _allUtxos;
+    UtxoItemList _allUtxos;
     WalletModel& _model;
-    Qt::SortOrder _sortOrder;
-    QString _sortRole;
 };
