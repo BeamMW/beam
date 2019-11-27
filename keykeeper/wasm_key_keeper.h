40ab85b2da7cabd496795bcedb4f622e80149cf3
Vadim Grigoruk <vadim@beam-mw.com>
2019-11-27 14:20:25 +0300

    merge with master

--- keykeeper/wasm_key_keeper.h
+++ keykeeper/wasm_key_keeper.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 #include "core/block_rw.h"
 #include "3rdparty/utilstrencodings.h"
 
