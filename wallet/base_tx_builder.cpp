00cf62bfb358585a6336de1f1544de5ef229bef4
chapati <chapati.th@gmail.com>
2019-11-27 20:33:42 +0700

    compilation error
    Took 27 seconds

--- wallet/base_tx_builder.cpp
+++ wallet/base_tx_builder.cpp
@@ -222,7 +222,7 @@ namespace beam::wallet
         //    {
         //        //m_Tx.Update();
         //    });
-        const auto& [commitments, ignore] = m_Tx.GetKeyKeeper()->GeneratePublicKeysSyncEx(m_InputCoins, true, m_AssetId);
+        const auto& [commitments, ignored] = m_Tx.GetKeyKeeper()->GeneratePublicKeysSyncEx(m_InputCoins, true, m_AssetId);
         m_Inputs.reserve(commitments.size());
         for (const auto& commitment : commitments)
         {
