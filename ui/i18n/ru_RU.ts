61e0eeeabec3cd86f10a19d4dd22a05cf2d12501
anatol <anatol@beam-mw.com>
2020-01-16 17:07:29 +0300

    #1172 sending by old addresses fixed

--- ui/i18n/ru_RU.ts
+++ ui/i18n/ru_RU.ts
@@ -540,10 +540,6 @@
         <source>Receive</source>
         <translation>Получить</translation>
     </message>
-    <message id="wallet-receive-my-addr-label">
-        <source>My address (auto-generated)</source>
-        <translation>Мой адрес</translation>
-    </message>
     <message id="wallet-receive-expires-label">
         <source>Expires in</source>
         <translation>Истекает через</translation>
@@ -1268,12 +1264,9 @@ Update your settings and try again.</source>
         <translation>Create swap offer</translation>
     </message>
     <message id="wallet-receive-addr-message">
-        <source>Send this address to the sender over an external secure channel or scan the QR code</source>
+        <source>Send this token to the sender over an external secure channel or scan the QR code</source>
+        <oldsource>Send this address to the sender over an external secure channel or scan the QR code</oldsource>
         <translation type="unfinished">Send this address to the sender over an external secure channel or scan the QR code</translation>
-    </message>
-    <message id="wallet-receive-copy-address">
-        <source>Copy transaction address</source>
-        <translation type="unfinished">Copy transaction address</translation>
     </message>
     <message id="receive-amount-swap-label">
         <source>Receive amount</source>
@@ -1511,7 +1504,7 @@ please review your settings and try again</source>
     </message>
     <message id="wallet-receive-copy-token">
         <source>copy swap token</source>
-        <oldsource>copy token</oldsource>
+        <oldsource>Copy transaction token</oldsource>
         <translation type="unfinished">copy transaction token</translation>
     </message>
     <message id="wallet-model-device-not-connected">
@@ -2064,16 +2057,36 @@ deploy the key at the node you trust completely.</translation>
         <extracomment>settings tab, general section, ask password to send, confirm password dialog, title if unchecked</extracomment>
         <translation type="unfinished"></translation>
     </message>
-    <message id="wallet-receive-use-token">
-        <source>Use identity token</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message id="wallet-receive-your-token">
         <source>Your receive token</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="wallet-receive-regular-copy-token">
-        <source>copy token</source>
+        <source>Copy transaction token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-kernel-min-height">
+        <source>Kernel&apos;s min height is unacceptable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-loopback">
+        <source>Not a loopback transaction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-key-keeper-no-initialized">
+        <source>Key keeper is not initialized</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-invalid-asset-id">
+        <source>No valid asset id/asset idx</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-invalid-asset-amount">
+        <source>Cannot consume more than MAX_INT64 asset groth in one transaction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="tx-failure-invalid-data-for-payment-proof">
+        <source>Some mandatory data for payment proof is missing</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
