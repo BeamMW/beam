b48792c6768c0b26344ce6cfe1a3047d322e7fe2
anatol <anatol@beam-mw.com>
2020-01-29 17:27:31 +0300

    #1264 added error message

--- ui/viewmodel/wallet/tx_object.cpp
+++ ui/viewmodel/wallet/tx_object.cpp
@@ -204,7 +204,7 @@ QString TxObject::getTransactionID() const
 
 QString TxObject::getReasonString(beam::wallet::TxFailureReason reason) const
 {
-    const std::array<QString,34> reasons = {
+    static const std::array<QString,34> reasons = {
         //% "Unexpected reason, please send wallet logs to Beam support"
         qtTrId("tx-failure-undefined"),
         //% "Transaction cancelled"
@@ -259,8 +259,10 @@ QString TxObject::getReasonString(beam::wallet::TxFailureReason reason) const
         qtTrId("tx-failure-loopback"),
         //% "Key keeper is not initialized"
         qtTrId("tx-failure-key-keeper-no-initialized"),
-        //% "No valid asset id/asset idx"
+        //% "No valid asset owner id/asset owner idx"
         qtTrId("tx-failure-invalid-asset-id"),
+        //%"Asset registration fee is too small"
+        qtTrId("tx-failure-asset-small-fee"),
         //% "Cannot consume more than MAX_INT64 asset groth in one transaction"
         qtTrId("tx-failure-invalid-asset-amount"),
         //% "Some mandatory data for payment proof is missing"
@@ -272,6 +274,7 @@ QString TxObject::getReasonString(beam::wallet::TxFailureReason reason) const
         //% "Aborted by the user"
         qtTrId("tx-failure-aborted-by-user"),
     };
+
     assert(reasons.size() > static_cast<size_t>(reason));
     return reasons[reason];
 }
