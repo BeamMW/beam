569d3ae08aee3a1b49ac4d477baa4bdb7d407832
sergey_zavarza <zavarza@beam-mw.com>
2020-03-21 00:28:45 +0300

    #1283 WIP tests params for travis

--- wallet/unittests/laser_test_utils.h
+++ wallet/unittests/laser_test_utils.h
@@ -29,8 +29,8 @@ const Height kLockTime = 5;
 const Height kPostLockReserve = 30;
 const Amount kFee = 100;
 const Height kOpenTxDh = 70;
-const Height kTestStartBlock = 10;
-const Height kNewBlockFunkStart = 8;
+const Height kTestStartBlock = 11;
+const Height kNewBlockFunkStart = 5;
 const unsigned kNewBlockInterval = 1000;
 
 struct LaserObserver : public laser::Mediator::Observer
