7cdaa0ea50ceab0c454c305287669bd175101e9b
anatol <anatol@beam-mw.com>
2020-04-01 13:51:01 +0300

    #1339 added coin selection test and fixed build

--- wallet/api/api_handler.cpp
+++ wallet/api/api_handler.cpp
@@ -457,7 +457,7 @@ namespace beam::wallet
             const auto txId = _walletData.getWallet().StartTransaction(CreateTransactionParameters(TxType::AssetIssue, data.txId)
                 .SetParameter(TxParameterID::Amount, data.value)
                 .SetParameter(TxParameterID::Fee, data.fee)
-                .SetParameter(TxParameterID::PreselectedCoins, coins);
+                .SetParameter(TxParameterID::PreselectedCoins, coins));
                 // TODO:ASSETS refactor
                 //.SetParameter(TxParameterID::AssetOwnerIdx, data.index));
 
