27aea26f1fbbb0d983504bd847e7a5d72ba411d2
Aleksandr Starun <alex.starun@beam-mw.com>
2020-04-11 18:40:35 +0300

    Reverted changes by a5bae4a9f1c47940d1048b6aa5aca16154ca6b46 that were used for tests lelantus

--- wallet/transactions/lelantus/pull_transaction.cpp
+++ wallet/transactions/lelantus/pull_transaction.cpp
@@ -24,19 +24,13 @@ namespace beam::wallet::lelantus
     TxParameters CreatePullTransactionParameters(const WalletID& myID, const boost::optional<TxID>& txId)
     {
         // TODO: find best place to init ShieldedInputMinCfg
-        // TODO: uncomment after lelantus's tests
-        /*const Lelantus::Cfg minInputCfg{ 4, 5 };*/
+        const Lelantus::Cfg minInputCfg{ 4, 5 };
 
         return CreateTransactionParameters(TxType::PullTransaction, txId)
             .SetParameter(TxParameterID::MyID, myID)
             .SetParameter(TxParameterID::IsSender, false)
-            // TODO: uncomment after lelantus's tests
-            //.SetParameter(TxParameterID::ShieldedInputCfg, Lelantus::Cfg{})
-            //.SetParameter(TxParameterID::ShieldedInputMinCfg, minInputCfg);
-
-            // TODO: remove after lelantus's tests
-            .SetParameter(TxParameterID::ShieldedInputCfg, Lelantus::Cfg{4,3})
-            .SetParameter(TxParameterID::ShieldedInputMinCfg, Lelantus::Cfg{4,2});
+            .SetParameter(TxParameterID::ShieldedInputCfg, Lelantus::Cfg{})
+            .SetParameter(TxParameterID::ShieldedInputMinCfg, minInputCfg);
     }
 
     BaseTransaction::Ptr PullTransaction::Creator::Create(INegotiatorGateway& gateway
