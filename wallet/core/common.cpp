ddf227809f1f5c0bef6980cfda38b36f839ee101
chapati <chapati.th@gmail.com>
2020-04-22 19:55:52 +0300

    #1227 - api test crash fix

--- wallet/core/common.cpp
+++ wallet/core/common.cpp
@@ -138,6 +138,17 @@ namespace beam
 
 namespace beam::wallet
 {
+    template<> bool fromByteBuffer<std::string>(const ByteBuffer& b, std::string& value)
+    {
+        if (b.empty()) return false;
+
+        Deserializer d;
+        d.reset(b.data(), b.size());
+        d & value;
+
+        return true;
+    }
+
     int WalletID::cmp(const WalletID& x) const
     {
         int n = m_Channel.cmp(x.m_Channel);
