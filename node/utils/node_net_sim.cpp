ffe1f70a7a6a8ca35ba8bcb7c11c8664b129d162
valdok <valdo@beam-mw.com>
2020-04-29 01:06:19 +0300

    node_net_sim WIP (1)

--- node/utils/node_net_sim.cpp
+++ node/utils/node_net_sim.cpp
@@ -15,6 +15,7 @@
 #include "../node.h"
 #include "../../mnemonic/mnemonic.h"
 #include "../../utility/cli/options.h"
+#include "../../core/fly_client.h"
 
 #define LOG_VERBOSE_ENABLED 0
 #include "utility/logger.h"
@@ -50,6 +51,77 @@ bool ReadSeed(Key::IKdf::Ptr& pKdf, const char* szSeed)
     return true;
 }
 
+struct Context
+{
+    Key::IKdf::Ptr m_pKdf;
+
+
+	struct MyFlyClient
+		:public proto::FlyClient
+//		,public proto::FlyClient::Request::IHandler
+	{
+
+		bool m_bRunning;
+		Height m_hRolledTo;
+
+		Block::SystemState::HistoryMap m_Hist;
+
+		//virtual void OnComplete(Request& r) override
+		//{
+		//
+		//}
+
+        virtual void OnNewTip() override
+        {
+            // tip already added
+        }
+
+        virtual void OnRolledBack() override
+        {
+            // reversed states are already removed
+            m_hRolledTo = m_Hist.m_Map.empty() ? 0 : m_Hist.m_Map.rbegin()->first;
+        }
+
+        virtual void get_Kdf(Key::IKdf::Ptr& pKdf) override
+        {
+            pKdf = get_ParentObj().m_pKdf;
+        }
+
+        virtual void get_OwnerKdf(Key::IPKdf::Ptr& pPKdf) override
+        {
+            pPKdf = get_ParentObj().m_pKdf;
+        }
+
+        virtual Block::SystemState::IHistory& get_History() override
+        {
+            return m_Hist;
+        }
+
+      
+        IMPLEMENT_GET_PARENT_OBJ(Context, m_FlyClient)
+
+    } m_FlyClient;
+
+
+
+
+
+
+
+
+};
+
+void DoTest(Key::IKdf::Ptr& pKdf)
+{
+    Node node;
+    node.m_Cfg.m_sPathLocal = "node_net_sim.db";
+    node.m_Keys.SetSingleKey(pKdf);
+    node.Initialize();
+
+    io::Reactor::get_Current().run();
+}
+
+
 } // namespace beam
 
 
@@ -60,7 +132,10 @@ int main(int argc, char* argv[])
 {
     using namespace beam;
 
-    auto [options, visibleOptions] = createOptionsDescription(GENERAL_OPTIONS);
+    auto [options, visibleOptions] = createOptionsDescription(0);
+    visibleOptions; // suppress warning
+    options.add_options()
+        (cli::SEED_PHRASE, po::value<std::string>()->default_value(""), "seed phrase");
 
     po::variables_map vm;
     try
@@ -73,13 +148,19 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    if (vm.count(cli::HELP))
+    Key::IKdf::Ptr pKdf;
+    if (!ReadSeed(pKdf, vm[cli::SEED_PHRASE].as<std::string>().c_str()))
     {
         std::cout << options << std::endl;
         return 0;
     }
 
     Rules::get().UpdateChecksum();
+
+    io::Reactor::Ptr pReactor(io::Reactor::create());
+    io::Reactor::Scope scope(*pReactor);
+
+    DoTest(pKdf);
 
     return 0;
 }
