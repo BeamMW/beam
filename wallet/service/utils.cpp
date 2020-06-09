afaf845cfd06b2b2d78d7bdc632d8500e1dda73b
chapati <chapati.th@gmail.com>
2020-06-10 01:57:56 +0300

    #1394 - compilation error fixed

--- wallet/service/utils.cpp
+++ wallet/service/utils.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <chrono>
-#include <iomanip>
+#include <sstream>
 #include "utils.h"
 
 namespace beam::wallet {
