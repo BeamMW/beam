dc6b1834d0003f70084a255afdf8927340683217
chapati <chapati.th@gmail.com>
2020-07-31 20:04:24 +0300

    MAC compilation fix

--- wallet/unittests/wallet_assets_test.cpp
+++ wallet/unittests/wallet_assets_test.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <boost/filesystem.hpp>
-#include "utility/logger.h"
 #include "wallet/core/common.h"
+#include "utility/logger.h"
 #include "wallet/core/wallet_network.h"
 #include "wallet/core/base_transaction.h"
 #include "wallet/core/simple_transaction.h"
