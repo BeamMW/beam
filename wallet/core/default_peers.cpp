efbc6b95d66e788f80778e7f75fe09d55bb664ed
anatol <sevostyanaa@gmail.com>
2020-11-05 17:41:42 +0300

    #266 updated default peers for BeamX

--- wallet/core/default_peers.cpp
+++ wallet/core/default_peers.cpp
@@ -29,6 +29,10 @@ namespace beam
             "us-nodes.mainnet.beam.mw:8100",
             "ap-hk-nodes.mainnet.beam.mw:8100",
             "shanghai-node.mainnet.beam.mw:8100"
+#elif defined(BEAM_BEAMX)
+            "54.176.159.202:8100",
+            "52.53.60.146:8100",
+            "54.241.38.13:8100"
 #else
             "eu-node01.masternet.beam.mw:8100",
             "eu-node02.masternet.beam.mw:8100",
