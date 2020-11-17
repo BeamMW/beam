b3513d1bda72844c7c4444778292cb7008bbaa6d
anatol <sevostyanaa@gmail.com>
2020-11-17 16:10:08 +0300

    fixed warning

--- wallet/core/common.cpp
+++ wallet/core/common.cpp
@@ -522,7 +522,7 @@ namespace beam::wallet
 
     bool TxToken::IsValid() const
     {
-        for (const auto p : m_Parameters)
+        for (const auto& p : m_Parameters)
         {
             switch (p.first)
             {
