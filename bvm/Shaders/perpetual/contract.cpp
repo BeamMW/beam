e7079164faa49bf80014aa7eb71a9a2031aee4c5
valdok <valdo@beam-mw.com>
2021-01-06 12:00:28 +0200

    Shaders: Perpetual updated

--- bvm/Shaders/perpetual/contract.cpp
+++ bvm/Shaders/perpetual/contract.cpp
@@ -4,8 +4,8 @@
 #include "../Math.h"
 #include "contract.h"
 
-const uint32_t nPerc_mp = 100000;
-const uint32_t nMargin_mp_max = 10 * nPerc_mp;
+const uint32_t g_nPerc_mp = 100000;
+const uint32_t nMargin_mp_max = 10 * g_nPerc_mp;
 
 export void Ctor(const Perpetual::Create& r)
 {
@@ -25,6 +25,15 @@ export void Dtor(void*)
 	Env::DelVar_T(key);
 }
 
+bool LoadAccount(const PubKey& pk, Perpetual::OfferState& s)
+{
+	if (Env::LoadVar_T(pk, s))
+		return true;
+
+	Utils::ZeroObject(s);
+	return false;
+}
+
 export void Method_2(const Perpetual::CreateOffer& r)
 {
 	uint8_t key = 0;
@@ -32,13 +41,22 @@ export void Method_2(const Perpetual::CreateOffer& r)
 	Env::LoadVar_T(key, g);
 
 	Perpetual::OfferState s;
-	Env::Halt_if(Env::LoadVar_T(r.m_Account, s));
+	LoadAccount(r.m_Account, s);
+	Env::Halt_if(s.m_Amount1 != 0); // ensure this user doesn't have the account yet
 
 	// check there's enough collateral
-	/// r.m_AmountBeam * (1 + g.m_MarginRequirement_mp / 100000) <= r.m_TotalBeams
+	// r.m_AmountBeam * (1 + g.m_MarginRequirement_mp / 100000) <= r.m_TotalBeams
+	// r.m_AmountBeam * (100000 + g.m_MarginRequirement_mp) <= r.m_TotalBeams * 100000
 
-	Utils::ZeroObject(s);
+	const uint32_t nAmountWords = sizeof(Amount) / sizeof(MultiPrecision::Word);
+
+	auto a = MultiPrecision::UInt<nAmountWords>(r.m_AmountBeam) * MultiPrecision::UInt<1>(g.m_MarginRequirement_mp + g_nPerc_mp);
+	auto b = MultiPrecision::UInt<nAmountWords>(r.m_TotalBeams) * MultiPrecision::UInt<1>(g_nPerc_mp);
+	Env::Halt_if(a > b);
+
 	s.m_Amount1 = r.m_TotalBeams;
+	s.m_AmountBeam = r.m_AmountBeam;
+	s.m_AmountToken = r.m_AmountToken;
 
 	Env::SaveVar_T(r.m_Account, s);
 }
