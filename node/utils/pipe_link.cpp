c8341b556218b778d792934051d8c9684b7037f5
valdok <valdo@beam-mw.com>
2021-01-26 16:10:07 +0200

    pipe_link WIP (1)

--- node/utils/pipe_link.cpp
+++ node/utils/pipe_link.cpp
@@ -19,20 +19,43 @@
 #include "../../core/treasury.h"
 #include "../../core/serialization_adapters.h"
 #include <boost/core/ignore_unused.hpp>
+#include "../../utility/logger_checkpoints.h"
 
 #define LOG_VERBOSE_ENABLED 0
 #include "utility/logger.h"
 
 namespace beam {
 
-uint16_t g_LocalNodePort = 17725;
-
 
 struct Manager
 {
-    std::mutex m_Mutex;
-    std::condition_variable m_Cmd;
-    std::condition_variable m_Response;
+    struct Event
+    {
+        std::mutex m_Mutex;
+
+        struct Client
+            :public boost::intrusive::list_base_hook<>
+        {
+            typedef intrusive::list<Client> List;
+            io::Reactor* m_pReactor = nullptr;
+        };
+
+        Event::Client::List m_lst;
+
+        struct Context
+        {
+            Event& m_Event;
+            std::unique_lock<std::mutex> m_Lock;
+
+            Context(Event&);
+
+            void Fire();
+            bool Wait();
+        };
+
+    private:
+        void RemoveStrict(Client&);
+    };
 
     struct PerChain
     {
@@ -40,21 +63,27 @@ struct Manager
         io::Reactor::Ptr m_pReactor;
         io::AsyncEvent::Ptr m_pEvent;
         std::thread m_Thread;
+        Node::Config m_Cfg;
         Node* m_pNode = nullptr;
         bool m_Stopped = false;
         bool m_Frozen = false;
+        Event m_Event;
     };
 
     bool m_Stop = false;
     bool m_Freeze = false;
 
+    std::mutex m_CtlMutex;
+    std::condition_variable m_CtlVar;
+
     struct Freezer
     {
         Manager& m_This;
-        std::unique_lock<std::mutex> m_Lock;
 
         Freezer(Manager&);
         ~Freezer();
+
+        void Do(bool);
     };
 
     PerChain m_pC[2];
@@ -69,6 +98,52 @@ struct Manager
     void OnEvent(uint32_t);
 };
 
+
+Manager::Event::Context::Context(Event& e)
+    :m_Event(e)
+    ,m_Lock(e.m_Mutex)
+{
+}
+
+void Manager::Event::Context::Fire()
+{
+    while (!m_Event.m_lst.empty())
+    {
+        Client& c = m_Event.m_lst.front();
+        assert(c.m_pReactor);
+        c.m_pReactor->stop();
+
+        m_Event.RemoveStrict(c);
+    }
+}
+
+void Manager::Event::RemoveStrict(Client& c)
+{
+    assert(c.m_pReactor);
+    c.m_pReactor = nullptr;
+    m_lst.pop_front();
+}
+
+bool Manager::Event::Context::Wait()
+{
+    io::Reactor& r = io::Reactor::get_Current();
+
+    Client c;
+    c.m_pReactor = &r;
+    m_Event.m_lst.push_back(c);
+
+    m_Lock.unlock();
+    r.run();
+    m_Lock.lock();
+
+    if (!c.m_pReactor)
+        return true;
+
+    m_Event.RemoveStrict(c);
+    r.stop();
+    return false;
+}
+
 bool Manager::Start()
 {
     for (uint32_t i = 0; i < _countof(m_pC); i++)
@@ -81,13 +156,11 @@ bool Manager::Start()
         c.m_Thread = std::thread(&Manager::RunInThread, this, i);
     }
 
-    std::unique_lock<std::mutex> scope(m_Mutex);
-
     for (uint32_t i = 0; i < _countof(m_pC); i++)
     {
         auto& c = m_pC[i];
 
-        while (true)
+        for (Event::Context ctx(c.m_Event); ; )
         {
             if (c.m_Stopped)
                 return false;
@@ -95,8 +168,10 @@ bool Manager::Start()
             if (c.m_pNode)
                 break;
 
-            m_Response.wait(scope);
+            if (!ctx.Wait())
+                return false;
         }
+
 
     }
 
@@ -122,9 +197,10 @@ void Manager::Stop()
 
 void Manager::RunInThread(uint32_t i)
 {
+//    CHECKPOINT("Pipe", i);
+
     auto& c = m_pC[i];
 
-    c.m_Rules.UpdateChecksum();
     Rules::Scope scopeRules(c.m_Rules);
 
     io::Reactor::Scope scopeReactor(*c.m_pReactor);
@@ -138,10 +214,10 @@ void Manager::RunInThread(uint32_t i)
         std::cout << e.what() << std::endl;
     }
 
-    std::unique_lock<std::mutex> scope(m_Mutex);
+    Event::Context ctx(c.m_Event);
     c.m_Stopped = true;
     c.m_pNode = nullptr;
-    m_Response.notify_one();
+    ctx.Fire();
 }
 
 void Manager::RunInThread2(uint32_t i)
@@ -149,20 +225,18 @@ void Manager::RunInThread2(uint32_t i)
     auto& c = m_pC[i];
 
     Node node;
+    node.m_Cfg = c.m_Cfg; // copy
+
+    //Key::IKdf::Ptr pKdf;
 
     node.m_Cfg.m_VerificationThreads = -1;
     node.m_Cfg.m_sPathLocal = "node_" + std::to_string(i) + ".db";
-
-    Key::IKdf::Ptr pKdf;
-
-    node.m_Cfg.m_Listen.port(g_LocalNodePort + i);
-    node.m_Cfg.m_Listen.ip(INADDR_ANY);
 
     // disable dandelion (faster tx propagation)
     node.m_Cfg.m_Dandelion.m_AggregationTime_ms = 0;
     node.m_Cfg.m_Dandelion.m_FluffProbability = 0xFFFF;
 
-    node.m_Keys.SetSingleKey(pKdf);
+    //node.m_Keys.SetSingleKey(pKdf);
     node.m_Cfg.m_Horizon.SetStdFastSync();
     node.Initialize();
 
@@ -191,9 +265,11 @@ void Manager::RunInThread2(uint32_t i)
     }
 
     {
-        std::unique_lock<std::mutex> scope(m_Mutex);
+        std::cout << "Pipe" << i << " sync complete" << std::endl;
+
+        Event::Context ctx(c.m_Event);
         c.m_pNode = &node;
-        m_Response.notify_one();
+        ctx.Fire();
     }
 
     c.m_pReactor->run();
@@ -201,33 +277,30 @@ void Manager::RunInThread2(uint32_t i)
 
 Manager::Freezer::Freezer(Manager& m)
     :m_This(m)
-    ,m_Lock(m.m_Mutex)
 {
-    m_This.m_Freeze = true;
-
-    for (uint32_t i = 0; i < _countof(m_This.m_pC); i++)
-        m_This.m_pC[i].m_pEvent->post();
-
-    for (uint32_t i = 0; i < _countof(m_This.m_pC); i++)
-    {
-        auto& c = m_This.m_pC[i];
-
-        while (c.m_pNode && !c.m_Frozen)
-            m_This.m_Response.wait(m_Lock);
-    }
+    Do(true);
 }
 
 Manager::Freezer::~Freezer()
 {
-    m_This.m_Freeze = false;
-    m_This.m_Cmd.notify_all();
+    Do(false);
+}
+
+void Manager::Freezer::Do(bool b)
+{
+    std::unique_lock<std::mutex> lock(m_This.m_CtlMutex);
+    m_This.m_Freeze = b;
+
+    m_This.m_CtlVar.notify_all();
 
     for (uint32_t i = 0; i < _countof(m_This.m_pC); i++)
     {
         auto& c = m_This.m_pC[i];
+        if (b)
+            c.m_pEvent->post();
 
-        while (c.m_pNode && c.m_Frozen)
-            m_This.m_Response.wait(m_Lock);
+        while ((c.m_Frozen != b) && c.m_pNode)
+            m_This.m_CtlVar.wait(lock);
     }
 }
 
@@ -235,29 +308,22 @@ void Manager::OnEvent(uint32_t i)
 {
     auto& c = m_pC[i];
 
-    std::unique_lock<std::mutex> scope(m_Mutex);
+    std::unique_lock<std::mutex> lock(m_CtlMutex);
 
     if (m_Freeze)
     {
         c.m_Frozen = true;
+        m_CtlVar.notify_all();
 
-        while (true)
-        {
-            m_Response.notify_one();
-            m_Cmd.wait(scope);
-
-            if (!m_Freeze || m_Stop)
-                break;
-        }
+        while (m_Freeze)
+            m_CtlVar.wait(lock);
 
         c.m_Frozen = false;
+        m_CtlVar.notify_all();
     }
 
     if (m_Stop)
         io::Reactor::get_Current().stop();
-
-    m_Response.notify_one();
-
 }
 
 } // namespace beam
@@ -279,11 +345,39 @@ int main_Guarded(int argc, char* argv[])
     {
         auto& c = man.m_pC[i];
         c.m_Rules = Rules::get(); // default
+
+        Rules::Scope scopeRules(c.m_Rules);
+
+        auto [options, visibleOptions] = createOptionsDescription(OptionsFlag::NODE_OPTIONS);
+        boost::ignore_unused(visibleOptions);
+        options.add_options()
+            (cli::SEED_PHRASE, po::value<std::string>()->default_value(""), "seed phrase")
+            ;
+
+        std::string sCfgPath = std::string("pipe_") + std::to_string(i) + ".cfg";
+        po::variables_map vm = getOptions(argc, argv, sCfgPath.c_str(), options);
+
         c.m_Rules.TreasuryChecksum = i;
         c.m_Rules.UpdateChecksum();
+
+        std::cout << "Pipe" << i << ", " << c.m_Rules.get_SignatureStr() << std::endl;
+
+        std::vector<std::string> vPeers = getCfgPeers(vm);
+
+        for (size_t iPeer = 0; iPeer < vPeers.size(); iPeer++)
+        {
+            io::Address addr;
+            if (addr.resolve(vPeers[iPeer].c_str()))
+                c.m_Cfg.m_Connect.push_back(addr);
+        }
+
     }
 
+    std::cout << "waiting both chains to sync..." << std::endl;
+
     man.Start();
+
+    std::cout << "Initial sync complete" << std::endl;
 
     {
         Manager::Freezer f(man);
