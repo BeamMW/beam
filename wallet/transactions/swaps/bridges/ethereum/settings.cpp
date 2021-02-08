66a0d9b2e0a92b5a8aa90624d65f70cbaad61a96
Roman Strilets <roman@beam-mw.com>
2021-02-08 10:34:08 +0300

    fixed infura url for mainnet

--- wallet/transactions/swaps/bridges/ethereum/settings.cpp
+++ wallet/transactions/swaps/bridges/ethereum/settings.cpp
@@ -25,8 +25,8 @@ namespace
     const std::string kUsdtContractAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
     const std::string kWBTCContractAddress = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
 
-    const std::string kEthNodeAddress = "infura.io:443";
-    const std::string kEthNodeHost = "infura.io";
+    const std::string kEthNodeAddress = "mainnet.infura.io:443";
+    const std::string kEthNodeHost = "mainnet.infura.io";
     const bool kNeedSsl = true;
     const std::string kPahtAndQuery = "/v3/";
 #else // MASTERNET and TESTNET
