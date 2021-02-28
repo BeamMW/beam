8e0fb4e5e046f0c4923475bef020f27774310928
valdok <valdo@beam-mw.com>
2021-02-28 11:05:48 +0200

    Shaders: added make_all.bat

---
+++ bvm/Shaders/make_all.bat
@@ -0,0 +1,17 @@
+call make_shader dummy/contract
+call make_shader faucet/contract
+call make_shader faucet/app
+call make_shader mirrorcoin/contract
+call make_shader mirrorcoin/app
+call make_shader oracle/contract
+call make_shader perpetual/contract
+call make_shader perpetual/app
+call make_shader pipe/contract
+call make_shader pipe/app
+call make_shader roulette/contract
+call make_shader roulette/app
+call make_shader sidechain/contract
+call make_shader sidechain/app
+call make_shader StableCoin/contract
+call make_shader vault/contract
+call make_shader vault/app
