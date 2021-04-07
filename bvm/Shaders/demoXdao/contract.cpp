dbf8562e099750bd353d6a011c4f42952397a5f4
romanov_ai <alex@justdrive.ai>
2021-04-07 12:36:01 +0300

    Shaders: demoXdao updated

--- bvm/Shaders/demoXdao/contract.cpp
+++ bvm/Shaders/demoXdao/contract.cpp
@@ -1,5 +1,6 @@
 #include "../common.h"
 #include "contract.h"
+#include "../Math.h"
 
 export void Ctor(const void*)
 {
@@ -26,5 +27,14 @@ export void Method_2(const DemoXdao::LockAndGet& r)
     Env::FundsLock(0, r.m_Amount);
     Env::FundsUnlock(s.m_Aid, s.s_ReleasePerLock);
     Env::AddSig(r.m_Pk);
+
+    Amount val;
+    if (Env::LoadVar_T(r.m_Pk, val))
+        Strict::Add(val, r.m_Amount);
+    else
+        val = r.m_Amount;
+
+    if (val)
+        Env::SaveVar_T(r.m_Pk, val);
 }
 
