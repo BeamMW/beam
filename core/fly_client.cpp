dcd4458ceae9de4d7c63e4b67b118ca4a9e1ef54
valdok <valdo@beam-mw.com>
2021-04-15 23:12:21 +0300

    FlyClient: misc (cosmetic)

--- core/fly_client.cpp
+++ core/fly_client.cpp
@@ -832,7 +832,7 @@ void FlyClient::NetworkStd::Connection::OnRequestData(RequestStateSummary& req)
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestEnumHdrs& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 8);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 8);
 }
 
 bool details::ExtraData<proto::HdrPack>::DecodeAndCheck(const HdrPack& msg)
@@ -927,7 +927,7 @@ void FlyClient::NetworkStd::Connection::OnMsg(DataMissing&& msg)
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestContractVars& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 8);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 8);
 }
 
 void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractVars& req)
@@ -936,7 +936,7 @@ void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractVars& req)
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestContractLogs& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 8);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 8);
 }
 
 void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractLogs& req)
@@ -945,7 +945,7 @@ void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractLogs& req)
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestContractVar& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 8);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 8);
 }
 
 void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractVar& req)
@@ -956,7 +956,7 @@ void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractVar& req)
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestContractLogProof& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 8);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 8);
 }
 
 void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractLogProof& req)
@@ -966,7 +966,7 @@ void FlyClient::NetworkStd::Connection::OnRequestData(RequestContractLogProof& r
 
 bool FlyClient::NetworkStd::Connection::IsSupported(RequestShieldedOutputsAt& req)
 {
-    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::Extension::get(m_LoginFlags) >= 7);
+    return (Flags::Node & m_Flags) && IsAtTip() && (LoginFlags::Extension::get(m_LoginFlags) >= 7);
 }
 
 void FlyClient::NetworkStd::Connection::OnRequestData(RequestShieldedOutputsAt& req)
