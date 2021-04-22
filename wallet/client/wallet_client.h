44821c54b47fe2c01b945940957b97f22aed79cd
chapati <chapati.th@gmail.com>
2021-04-22 21:47:26 +0300

    clang build fix

--- wallet/client/wallet_client.h
+++ wallet/client/wallet_client.h
@@ -224,7 +224,7 @@ namespace beam::wallet
         void getAddress(const WalletID& addr) override;
         void getAddress(const WalletID& addr, AsyncCallback<const boost::optional<WalletAddress>&, size_t>&& callback) override;
         void getAddressByToken(const std::string& token, AsyncCallback<const boost::optional<WalletAddress>&, size_t>&& callback) override;
-        void deleteAddressByToken(const std::string& token);
+        void deleteAddressByToken(const std::string& token) override;
         void saveVouchers(const ShieldedVoucherList& v, const WalletID& walletID) override;
         void setNodeAddress(const std::string& addr) override;
         void changeWalletPassword(const SecString& password) override;
