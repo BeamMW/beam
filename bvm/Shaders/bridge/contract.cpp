23a39f53f56ac0e3e5c55413014f9e261c30d066
roman-strilets <roman@beam-mw.com>
2021-06-25 12:26:15 +0300

    check result of receipt proof

--- bvm/Shaders/bridge/contract.cpp
+++ bvm/Shaders/bridge/contract.cpp
@@ -4,6 +4,21 @@
 #include "../Ethash.h"
 #include "../Eth.h"
 
+namespace
+{
+struct RlpVisitor
+{
+    bool OnNode(const Eth::Rlp::Node& node)
+    {
+        auto& item = m_Items.emplace_back();
+        item = node;
+        return false;
+    }
+
+    Utils::Vector<Eth::Rlp::Node> m_Items;
+};
+} // namespace
+
 // Method_0 - constructor, called once when the contract is deployed
 export void Ctor(void*)
 {
@@ -64,6 +79,40 @@ export void Method_4(Bridge::ImportMessage& value)
     uint8_t* pathInNibbles = (uint8_t*)Env::StackAlloc(nibblesLength);
     Eth::TriePathToNibbles(trieKey, value.m_TrieKeySize, pathInNibbles, nibblesLength);
     Env::Halt_if(!Eth::VerifyEthProof(pathInNibbles, nibblesLength, receiptProof, value.m_ReceiptProofSize, value.m_Header.m_ReceiptHash, &out, size));
+
+    // only for DummyContract
+    RlpVisitor v0;
+    Env::Halt_if(!Eth::Rlp::Decode(out, size, v0));
+    RlpVisitor v1;
+    Env::Halt_if(!Eth::Rlp::Decode(v0.m_Items.m_p[0].m_pBuf, v0.m_Items.m_p[0].m_nLen, v1));
+    RlpVisitor v2;
+    Env::Halt_if(!Eth::Rlp::Decode(v1.m_Items.m_p[v1.m_Items.m_Count - 1].m_pBuf, v1.m_Items.m_p[v1.m_Items.m_Count - 1].m_nLen, v2));
+    RlpVisitor v3;
+    Env::Halt_if(!Eth::Rlp::Decode(v2.m_Items.m_p[v2.m_Items.m_Count - 1].m_pBuf, v2.m_Items.m_p[v2.m_Items.m_Count - 1].m_nLen, v3));
+
+    // check only data
+    const Eth::Rlp::Node& node = v3.m_Items.m_p[v3.m_Items.m_Count - 1];
+    const uint8_t* idx = node.m_pBuf;
+
+    // look at https://docs.soliditylang.org/en/v0.5.3/abi-spec.html#events
+    // check buffer size 
+    Env::Halt_if(node.m_nLen != 192);
+
+    // check amount
+    idx += 32;
+    Opaque<32> rawNumber;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> amount;
+    amount.FromBE_T(rawNumber);
+    MultiPrecision::UInt<8> expectedAmount;
+    expectedAmount = value.m_Msg.m_Amount;
+    Env::Halt_if(amount.cmp(expectedAmount));
+
+    // check public key
+    idx += 96;
+    PubKey pubKey;
+    Env::Memcpy(&pubKey, idx, sizeof(pubKey));
+    Env::Halt_if(Env::Memcmp(&pubKey, &value.m_Msg.m_Pk, sizeof(pubKey)));
 
     uint32_t key = 1;
     Bridge::InMsg tmp = value.m_Msg;
