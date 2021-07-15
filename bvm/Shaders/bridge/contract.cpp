263d6a4f1ca24248e96468aa12894ace4c52af30
roman-strilets <roman@beam-mw.com>
2021-07-15 16:31:54 +0300

    check proof result

--- bvm/Shaders/bridge/contract.cpp
+++ bvm/Shaders/bridge/contract.cpp
@@ -4,6 +4,159 @@
 #include "../Ethash.h"
 #include "../Eth.h"
 
+namespace
+{
+struct RlpVisitor
+{
+    bool OnNode(const Eth::Rlp::Node& node)
+    {
+        auto& item = m_Items.emplace_back();
+        item = node;
+        return false;
+    }
+
+    Utils::Vector<Eth::Rlp::Node> m_Items;
+};
+
+bool FindBridgeLog(const uint8_t* buffer, uint32_t bufferSize, const uint8_t** data, uint32_t& dataSize)
+{
+    const uint8_t expectedTopic[] = { 160,17,220,234,188,149,79,103,20,251,148,168,222,215,64,236,190,17,46,241,182,194,35,31,163,69,9,69,158,54,52,85 };
+    RlpVisitor v0;
+
+    Env::Halt_if(!Eth::Rlp::Decode(buffer, bufferSize, v0));
+    Env::Halt_if(v0.m_Items.m_Count != 1);
+    Env::Halt_if(v0.m_Items.m_p[0].m_Type != Eth::Rlp::Node::Type::List);
+
+    RlpVisitor v1;
+    Env::Halt_if(!Eth::Rlp::Decode(v0.m_Items.m_p[0].m_pBuf, (uint32_t)v0.m_Items.m_p[0].m_nLen, v1));
+    Env::Halt_if(v1.m_Items.m_Count != 4);
+    Env::Halt_if(v1.m_Items.m_p[v1.m_Items.m_Count - 1].m_Type != Eth::Rlp::Node::Type::List);
+
+    // get event list
+    RlpVisitor v2;
+    Env::Halt_if(!Eth::Rlp::Decode(v1.m_Items.m_p[v1.m_Items.m_Count - 1].m_pBuf, v1.m_Items.m_p[v1.m_Items.m_Count - 1].m_nLen, v2));
+    Env::Halt_if(v2.m_Items.m_Count == 0);
+    
+    for (uint32_t idx = 0; idx < v2.m_Items.m_Count; ++idx)
+    {
+        RlpVisitor eventNode;
+        Env::Halt_if(!Eth::Rlp::Decode(v2.m_Items.m_p[idx].m_pBuf, v2.m_Items.m_p[idx].m_nLen, eventNode));
+        Env::Halt_if(eventNode.m_Items.m_Count != 3);
+
+        RlpVisitor topics;
+        Env::Halt_if(!Eth::Rlp::Decode(eventNode.m_Items.m_p[1].m_pBuf, eventNode.m_Items.m_p[1].m_nLen, topics));
+
+        if (topics.m_Items.m_Count != 1) continue;
+        if (topics.m_Items.m_p[0].m_nLen != 32) continue;
+        if (!Env::Memcmp(topics.m_Items.m_p[0].m_pBuf, expectedTopic, 32))
+        {
+            dataSize = eventNode.m_Items.m_p[2].m_nLen;
+            *data = eventNode.m_Items.m_p[2].m_pBuf;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CheckBridgeLog(const uint8_t* data, uint32_t dataSize, const Bridge::PushRemote& pushRemote, const uint8_t* msgBody, uint32_t msgBodySize)
+{
+    if (dataSize <= 160) return false;
+
+    const uint8_t* idx = data;
+
+    // check msgId
+    Opaque<32> rawNumber;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> msgId;
+    msgId.FromBE_T(rawNumber);
+    if (msgId.get_Val<1>() != pushRemote.m_MsgId) return false;
+
+    // check sender address
+    idx += 32;
+    //Opaque<20> senderAddress;
+    //Env::Memcpy(&senderAddress, idx + 12, 20);
+    if (Env::Memcmp(idx + 12, &pushRemote.m_MsgHdr.m_ContractSender, 20)) return false;
+
+    // check receiver address
+    idx += 32;
+    /*Opaque<32> receiverAddress;
+    Env::Memcpy(&receiverAddress, idx, 32);*/
+    if (Env::Memcmp(idx, &pushRemote.m_MsgHdr.m_ContractReceiver, 32)) return false;
+
+    // check offset
+    idx += 32;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> offset;
+    offset.FromBE_T(rawNumber);
+    MultiPrecision::UInt<8> expectedOffset;
+    expectedOffset = 128u;
+    if (offset.cmp(expectedOffset)) return false;
+
+    // check msg size
+    idx += 32;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> size;
+    size.FromBE_T(rawNumber);
+    if (size.get_Val<1>() != msgBodySize) return false;
+
+    // check msg
+    idx += 32;
+    /*uint8_t* tmp = (uint8_t*)Env::StackAlloc(msgBodySize);
+    Env::Memcpy(tmp, idx, msgBodySize);*/
+    if (Env::Memcmp(idx, msgBody, msgBodySize)) return false;
+
+    return true;
+}
+
+/*void CheckBridgeLog(const uint8_t* data, uint32_t dataSize, const Bridge::PushRemote& pushRemote, const uint8_t* msgBody, uint32_t msgBodySize)
+{
+    Env::Halt_if(dataSize <= 160);
+
+    const uint8_t* idx = data;
+
+    // check msgId
+    Opaque<32> rawNumber;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> msgId;
+    msgId.FromBE_T(rawNumber);
+    Env::Halt_if(msgId.get_Val<1>() != pushRemote.m_MsgId);
+
+    // check sender address
+    idx += 32;
+    Opaque<20> senderAddress;
+    Env::Memcpy(&senderAddress, idx + 12, 20);
+    Env::Halt_if(Env::Memcmp(&senderAddress, &pushRemote.m_MsgHdr.m_ContractSender, 20));
+
+    // check receiver address
+    idx += 32;
+    Opaque<32> receiverAddress;
+    Env::Memcpy(&receiverAddress, idx, 32);
+    Env::Halt_if(Env::Memcmp(&receiverAddress, &pushRemote.m_MsgHdr.m_ContractReceiver, 32));
+
+    // check offset
+    idx += 32;
+    Env::Memcpy(&rawNumber, idx, 32);
+    MultiPrecision::UInt<8> offset;
+    offset.FromBE_T(rawNumber);
+    MultiPrecision::UInt<8> expectedOffset;
+    expectedOffset = 128u;
+    Env::Halt_if(offset.cmp(expectedOffset));
+
+    //// check msg size
+    //idx += 32;
+    //Env::Memcpy(&rawNumber, idx, 32);
+    //MultiPrecision::UInt<8> size;
+    //size.FromBE_T(rawNumber);
+    //Env::Halt_if(size.get_Val<1>() != msgBodySize);
+
+    //// check msg
+    //idx += 32;
+    //uint8_t* tmp = (uint8_t*)Env::StackAlloc(msgBodySize);
+    //Env::Memcpy(tmp, idx, msgBodySize);
+    //Env::Halt_if(Env::Memcmp(tmp, msgBody, msgBodySize));
+}*/
+} // namespace
+
 // Method_0 - constructor, called once when the contract is deployed
 export void Ctor(void*)
 {
@@ -57,9 +210,15 @@ export void Method_3(const Bridge::PushRemote& value)
     Env::Halt_if(!Eth::VerifyEthProof(pathInNibbles, nibblesLength, receiptProof, value.m_ReceiptProofSize, value.m_Header.m_ReceiptHash, &out, size));
 
     // TODO: add validation of the msgData
+    const uint8_t* data = nullptr;
+    uint32_t dataSize = 0;
+    Env::Halt_if(!FindBridgeLog(out, size, &data, dataSize));
 
     // save msg
     const uint8_t* msgBody = trieKey + value.m_TrieKeySize;
+
+    Env::Halt_if(!CheckBridgeLog(data, dataSize, value, msgBody, value.m_MsgSize));
+
     uint32_t fullMsgSize = value.m_MsgSize + sizeof(Bridge::RemoteMsgHdr);
     auto* pMsg = (Bridge::RemoteMsgHdr*)Env::StackAlloc(fullMsgSize);
 
