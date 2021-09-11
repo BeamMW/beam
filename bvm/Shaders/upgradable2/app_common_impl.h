a641685167f127d831d94d6559f676f52afdb813
valdok <valdo@beam-mw.com>
2021-09-11 12:01:00 +0300

    Shaders: added upgradable2: added MultiSigRitual

--- bvm/Shaders/upgradable2/app_common_impl.h
+++ bvm/Shaders/upgradable2/app_common_impl.h
@@ -185,4 +185,248 @@ struct ManagerUpgadable2
 		}
 	};
 
+
+	struct MultiSigRitual
+	{
+	#pragma pack (push, 1)
+		// <-
+		struct Msg1
+		{
+			PubKey m_pkMyNonce;
+		};
+		// ->
+		struct Msg2
+		{
+			Height m_hMin;
+			PubKey m_pkKrnBlind;
+			PubKey m_pkFullNonce;
+		};
+		// <-
+		struct Msg3
+		{
+			Secp_scalar_data m_kMySig;
+		};
+	#pragma pack (pop)
+
+		Msg2 m_Msg2;
+
+		static const Height s_dh = 20;
+
+		static const uint32_t s_iSlotNonceKey = 0;
+		static const uint32_t s_iSlotKrnNonce = 1;
+		static const uint32_t s_iSlotKrnBlind = 2;
+
+		Env::KeyID m_Kid;
+		const ContractID* m_pCid;
+		uint32_t m_iMethod;
+		Upgradable2::Control::Signed* m_pArg;
+		uint32_t m_nArg;
+		const char* m_szComment;
+		PubKey m_pPks[Upgradable2::Settings::s_AdminsMax];
+		Secp_scalar_data m_pE[Upgradable2::Settings::s_AdminsMax];
+		uint32_t m_nPks;
+
+		void InvokeKrn(const Secp_scalar_data& kSig, Secp_scalar_data* pE)
+		{
+			Env::GenerateKernelAdvanced(
+				m_pCid, m_iMethod, m_pArg, m_nArg, nullptr, 0, m_pPks, m_nPks, m_szComment, 0,
+				m_Msg2.m_hMin, m_Msg2.m_hMin + s_dh, m_Msg2.m_pkKrnBlind, m_Msg2.m_pkFullNonce, kSig, s_iSlotKrnBlind, s_iSlotKrnNonce, pE);
+		}
+
+		void GetChallenges()
+		{
+			InvokeKrn(*m_pE, m_pE);
+		}
+
+		void Finalize(const Secp_scalar_data& kSig)
+		{
+			InvokeKrn(kSig, nullptr);
+		}
+
+		static bool InMask(uint32_t msk, uint32_t i)
+		{
+			return !!(1 & (msk >> i));
+		}
+
+		void Perform(const Upgradable2::Settings& stg)
+		{
+			uint32_t msk = 0, iSender = 0;
+			Env::DocGet("approve_mask", msk);
+			Env::DocGet("iSender", iSender);
+
+			if (iSender >= stg.s_AdminsMax)
+				return OnError("iSender oob");
+			if (!InMask(msk, iSender))
+				return OnError("iSender isn't in mask");
+
+			PubKey pkMy;
+			m_Kid.get_Pk(pkMy);
+
+			m_nPks = 0;
+			uint32_t iAdmin = 0, iAdminKeyIdx = 0;
+
+			m_pArg->m_ApproveMask = msk;
+
+			for (uint32_t i = 0; i < stg.s_AdminsMax; i++)
+			{
+				if (InMask(msk, i))
+				{
+					const PubKey& pk = stg.m_pAdmin[i];
+					if (_POD_(pk).IsZero())
+						return OnError("some approvers aren't valid");
+
+					if (_POD_(pk) == pkMy)
+					{
+						iAdminKeyIdx = m_nPks;
+						iAdmin = i + 1;
+					}
+
+					m_nPks++;
+				}
+			}
+
+			if (m_nPks < stg.m_MinApprovers)
+				return OnError("not enough approvers");
+
+
+			if (!iAdmin)
+				return OnError("not admin");
+			iAdmin--;
+
+			HashProcessor::Sha256 hpContext;
+			hpContext.Write(m_pArg, m_nArg);
+			hpContext << m_iMethod << msk << iSender;
+
+			Secp::Point p0;
+			p0.FromSlot(s_iSlotNonceKey);
+
+			Height h = Env::get_Height();
+
+			if (iAdmin == iSender)
+			{
+				m_Msg2.m_hMin = h + 1;
+
+				Comm::Channel pPeers[stg.s_AdminsMax];
+
+				// setup channels
+				for (uint32_t iPeer = 0; iPeer < stg.s_AdminsMax; iPeer++)
+				{
+					if (!InMask(msk, iPeer))
+						continue;
+					if (iPeer == iAdmin)
+						continue;
+
+					auto& cc = pPeers[iPeer];
+
+					cc.Init(m_Kid, stg.m_pAdmin[iPeer], &hpContext);
+					cc.Expose(iPeer);
+					cc.RcvStart();
+				}
+
+				Secp::Point p1;
+
+				for (uint32_t iPeer = 0; iPeer < stg.s_AdminsMax; iPeer++)
+				{
+					if (!InMask(msk, iPeer))
+						continue;
+					if (iPeer == iAdmin)
+						continue;
+
+					auto& cc = pPeers[iPeer];
+					auto& msg1 = cc.Rcv_T<Msg1>("waiting for co-signer nonce", false);
+
+					if (!p1.Import(msg1.m_pkMyNonce))
+						return OnError("bad nonce");
+
+					p0 += p1;
+				}
+
+				p0.Export(m_Msg2.m_pkFullNonce);
+
+				p0.FromSlot(s_iSlotKrnBlind);
+				p0.Export(m_Msg2.m_pkKrnBlind);
+
+				GetChallenges();
+
+				for (uint32_t iPeer = 0; iPeer < stg.s_AdminsMax; iPeer++)
+				{
+					if (!InMask(msk, iPeer))
+						continue;
+					if (iPeer == iAdmin)
+						continue;
+
+					auto& cc = pPeers[iPeer];
+
+					cc.Send_T(m_Msg2);
+					cc.RcvStart();
+				}
+
+				Secp::Scalar kSig, e;
+				m_nPks = 0;
+
+				for (uint32_t iPeer = 0; iPeer < stg.s_AdminsMax; iPeer++)
+				{
+					if (!InMask(msk, iPeer))
+						continue;
+
+					e.Import(m_pE[m_nPks++]);
+
+					if (iPeer == iAdmin)
+						m_Kid.get_Blind(e, e, s_iSlotNonceKey);
+					else
+					{
+						auto& cc = pPeers[iPeer];
+						auto& msg3 = cc.Rcv_T<Msg3>("waiting for co-signer sig", false);
+
+						if (!e.Import(msg3.m_kMySig))
+							return OnError("bad sig");
+					}
+
+					kSig += e;
+				}
+
+				Secp_scalar_data kSigData;
+				kSig.Export(kSigData);
+
+				Finalize(kSigData); // get all challenges
+			}
+			else
+			{
+				Comm::Channel cc(m_Kid, stg.m_pAdmin[iSender], &hpContext);
+				cc.Expose(iAdmin);
+
+				Msg1 msg1;
+				p0.Export(msg1.m_pkMyNonce);
+				cc.Send_T(msg1);
+
+				cc.Rcv_T(m_Msg2, "waiting for sender");
+
+				if ((m_Msg2.m_hMin + 10 < h) || (m_Msg2.m_hMin >= h + 20))
+					OnError("height insane");
+
+				GetChallenges();
+
+				Secp::Scalar kSig;
+				kSig.Import(m_pE[iAdminKeyIdx]);
+				m_Kid.get_Blind(kSig, kSig, s_iSlotNonceKey);
+
+				Msg3 msg3;
+				kSig.Export(msg3.m_kMySig);
+				cc.Send_T(msg3);
+
+				Env::DocAddText("", "Negotiation is over");
+			}
+		}
+
+		void Perform()
+		{
+			Upgradable2::Settings stg;
+			if (!ReadSettings(stg, *m_pCid))
+				return OnError("no settings");
+
+			Perform(stg);
+
+		}
+	};
+
 };
