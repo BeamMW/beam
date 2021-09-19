1eb6cb50016f42799453db188812c61c85d594e3
anatol <anatol@beam-mw.com>
2021-09-19 23:17:54 +0300

    changed paths in GenerateSID

--- cmake/GenerateSID.cmake
+++ cmake/GenerateSID.cmake
@@ -9,9 +9,9 @@ if (NOT TARGET sid_generator)
                                     -DBEAM_ATOMIC_SWAP_SUPPORT=FALSE
                                     -DBEAM_WALLET_CLIENT_LIBRARY=TRUE
                                     -DBEAM_TESTS_ENABLED=FALSE
-                                    -S ${PROJECT_SOURCE_DIR}/beam
-                                    -B ${PROJECT_BINARY_DIR}/beam
-        COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/beam --parallel --target generate-sid --config ${BEAM_SIDGEN_BUILD_TYPE}
+                                    -S ${BEAM_PROJECT_SOURCE_DIR}
+                                    -B ${BEAM_PROJECT_BINARY_DIR}/beam
+        COMMAND ${CMAKE_COMMAND} --build ${BEAM_PROJECT_BINARY_DIR}/beam --parallel --target generate-sid --config ${BEAM_SIDGEN_BUILD_TYPE}
         COMMENT "Building SID generator..."
         VERBATIM
     )
@@ -20,7 +20,7 @@ endif()
 function(generate_sid_header shader_target)
     add_custom_target(${shader_target}_header
         ALL
-        COMMAND ${PROJECT_BINARY_DIR}/beam/bvm/sid_generator/${BEAM_SIDGEN_BUILD_TYPE}/generate-sid $<TARGET_FILE:${shader_target}> > $<TARGET_PROPERTY:${shader_target},SOURCE_DIR>/contract_sid.i
+        COMMAND ${BEAM_PROJECT_BINARY_DIR}/beam/bvm/sid_generator/${BEAM_SIDGEN_BUILD_TYPE}/generate-sid $<TARGET_FILE:${shader_target}> > $<TARGET_PROPERTY:${shader_target},SOURCE_DIR>/contract_sid.i
         #COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${shader_target}> $<TARGET_PROPERTY:${shader_target},SOURCE_DIR>/$<TARGET_FILE_NAME:${shader_target}>
         COMMENT "Generating SID..."
         DEPENDS sid_generator
