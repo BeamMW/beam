f17bbb432242efb3787bb49d6aaf7fb7746d172b
Denis Demyanko <denis.d@it-dimension.com>
2021-10-22 15:35:08 +0300

    android -> transaction info

--- android/com/mw/beam/beamwallet/core/entities/dto/TxDescriptionDTO.java
+++ android/com/mw/beam/beamwallet/core/entities/dto/TxDescriptionDTO.java
@@ -40,4 +40,6 @@ public class TxDescriptionDTO
 	public String appName;
 	public String appID;
 	public String contractCids;
+	public String minConfirmations;
+	public String minConfirmationsProgress;	
 }
