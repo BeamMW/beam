e3f6f0913bb3217262b1626859abb1c61d43f578
valdok <valdo@beam-mw.com>
2021-11-16 16:03:35 +0200

    Revert "bvm: wasm logs on"
    This reverts commit df12c499b759ee6b7aea31bb9e801e3636c6917a.

--- bvm/unittest/shaders_test.cpp
+++ bvm/unittest/shaders_test.cpp
@@ -1136,12 +1136,9 @@ namespace bvm2 {
 			args.m_RulesCfg = r.pForks[2].m_Hash;
 
 			Dbg dbg = m_Dbg;
-			m_Dbg.m_Instructions = true;
-			m_Dbg.m_Stack = true;
-			m_Dbg.m_ExtCall = true;
-
-			std::ostringstream os;
-			m_Dbg.m_pOut = &os;
+			m_Dbg.m_Instructions = false;
+			m_Dbg.m_Stack = false;
+			m_Dbg.m_ExtCall = false;
 
 			verify_test(RunGuarded_T(cid, args.s_iMethod, args));
 
