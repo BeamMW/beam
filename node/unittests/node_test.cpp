48ed2908746324b980a33fe19700782f1fa55967
valdok <valdo@beam-mw.com>
2021-11-23 18:11:36 +0200

    build fix

--- node/unittests/node_test.cpp
+++ node/unittests/node_test.cpp
@@ -1061,7 +1061,7 @@ namespace beam
 				HeightRange hr(np.m_Cursor.m_ID.m_Height + 1, MaxHeight);
 
 				uint32_t nBvmCharge = 0;
-				verify_test(proto::TxStatus::Ok == np.ValidateTxContextEx(*pTx, hr, false, nBvmCharge, nullptr));
+				verify_test(proto::TxStatus::Ok == np.ValidateTxContextEx(*pTx, hr, false, nBvmCharge, nullptr, nullptr));
 
 				Transaction::Context::Params pars;
 				Transaction::Context ctx(pars);
