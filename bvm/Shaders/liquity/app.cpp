e92287556ed8b2bb2685e15098e2b960617d62f9
valdok <valdo@beam-mw.com>
2021-12-14 00:00:18 +0200

    Shaders/liquity: app WIP

--- bvm/Shaders/liquity/app.cpp
+++ bvm/Shaders/liquity/app.cpp
@@ -16,9 +16,11 @@
     macro(manager, my_admin_key) \
 
 #define Liquity_user_my_key(macro) macro(ContractID, cid)
+#define Liquity_user_my_trove(macro) macro(ContractID, cid)
 
 #define LiquityRole_user(macro) \
-    macro(user, my_key)
+    macro(user, my_key) \
+    macro(user, my_trove)
 
 #define LiquityRoles_All(macro) \
     macro(manager) \
@@ -126,6 +128,125 @@ struct MyGlobal
     }
 };
 
+struct MyGlobalPlusTroves
+    :public MyGlobal
+{
+    const ContractID& m_Cid;
+
+    MyGlobalPlusTroves(const ContractID& cid) :m_Cid(cid) {}
+
+    template <uint8_t nTag>
+    struct EpochStorage
+    {
+        const ContractID& m_Cid;
+        EpochStorage(const ContractID& cid) :m_Cid(cid) {}
+
+        void Load(uint32_t iEpoch, ExchangePool::Epoch& e)
+        {
+            Env::Key_T<EpochKey> k;
+            _POD_(k.m_Prefix.m_Cid) = m_Cid;
+            k.m_KeyInContract.m_Tag = nTag;
+            k.m_KeyInContract.m_iEpoch = iEpoch;
+
+            Env::Halt_if(!Env::VarReader::Read_T(k, e));
+        }
+
+        static void Save(uint32_t iEpoch, const ExchangePool::Epoch& e) {}
+        static void Del(uint32_t iEpoch) {}
+    };
+
+
+    Trove* m_pT = nullptr;
+    uint32_t m_ActiveTroves = 0;
+
+    struct My 
+    {
+        Trove* m_pT = nullptr;
+        Trove::ID m_iT;
+        uint32_t m_Index;
+        PubKey m_Pk;
+    } m_My;
+
+    Trove& get_T(Trove::ID iTrove)
+    {
+        assert(iTrove && (iTrove <= m_Troves.m_iLastCreated));
+        return m_pT[iTrove - 1];
+    }
+
+    ~MyGlobalPlusTroves()
+    {
+        if (m_pT)
+            Env::Heap_Free(m_pT);
+    }
+
+    void LoadAllTroves()
+    {
+        assert(!m_pT);
+        uint32_t nSize = sizeof(Trove) * m_Troves.m_iLastCreated;
+        m_pT = (Trove*)Env::Heap_Alloc(nSize);
+        Env::Memset(m_pT, 0, nSize);
+
+        Env::Key_T<Trove::Key> k0, k1;
+        _POD_(k0.m_Prefix.m_Cid) = m_Cid;
+        _POD_(k1.m_Prefix.m_Cid) = m_Cid;
+        k0.m_KeyInContract.m_iTrove = 0;
+        k1.m_KeyInContract.m_iTrove = static_cast<Trove::ID>(-1);
+
+        for (Env::VarReader r(k0, k1); ; m_ActiveTroves++)
+        {
+            Trove t;
+            if (!r.MoveNext_T(k0, t))
+                break;
+
+            _POD_(get_T(k0.m_KeyInContract.m_iTrove)) = t;
+        }
+    }
+
+    bool Load()
+    {
+        if (!MyGlobal::Load(m_Cid))
+            return false;
+
+        if (m_Troves.m_iLastCreated)
+            LoadAllTroves();
+
+        return true;
+    }
+
+    bool PopMyTrove(const TroveKeyID& kid)
+    {
+        assert(!m_My.m_pT);
+        kid.get_Pk(m_My.m_Pk);
+
+        Trove* pP = nullptr;
+        for (m_My.m_iT = m_Troves.m_iHead; m_My.m_iT; m_My.m_Index++)
+        {
+            Trove& t = get_T(m_My.m_iT);
+            if (_POD_(m_My.m_Pk) == t.m_pkOwner)
+            {
+                if (pP)
+                    pP->m_iNext = t.m_iNext;
+                else
+                    m_Troves.m_iHead = t.m_iNext;
+
+                EpochStorage<Tags::s_Epoch_Redist> stor(m_Cid);
+                m_RedistPool.Remove(t, stor);
+
+                m_Troves.m_Totals.Tok -= t.m_Amounts.Tok;
+                m_Troves.m_Totals.Col -= t.m_Amounts.Col;
+
+                m_My.m_pT = &t;
+                return true;
+            }
+
+            pP = &t;
+            m_My.m_iT = t.m_iNext;
+        }
+        return false;
+    }
+};
+
+
 void DocAddPair(const char* sz, const Pair& p)
 {
     Env::DocGroup gr(sz);
@@ -167,7 +288,6 @@ ON_METHOD(manager, view_params)
     DocAddPerc("baserate", g.m_BaseRate.m_k);
     Env::DocAddNum("stab_pool", g.m_StabPool.get_TotalSell());
     Env::DocAddNum("gov_pool", g.m_ProfitPool.m_Weight);
-
 }
 
 ON_METHOD(manager, my_admin_key)
@@ -183,6 +303,25 @@ ON_METHOD(user, my_key)
     PubKey pk;
     TroveKeyID(cid).get_Pk(pk);
     Env::DocAddBlob_T("key", pk);
+}
+
+ON_METHOD(user, my_trove)
+{
+    MyGlobalPlusTroves g(cid);
+    if (!g.Load())
+        return;
+
+    Env::DocGroup gr("res");
+    Env::DocAddNum("nTotal", g.m_ActiveTroves);
+
+    TroveKeyID kid(cid);
+    if (g.PopMyTrove(kid))
+    {
+        auto& t = *g.m_My.m_pT;
+
+        Env::DocAddNum("iPos", g.m_My.m_Index);
+        DocAddPair("amounts", t.m_Amounts);
+    }
 }
 
 #undef ON_METHOD
