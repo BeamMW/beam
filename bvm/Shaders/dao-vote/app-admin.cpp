b9d14c5f15237c711ce03a3b4bb860aa666e8cb0
valdok <valdo@beam-mw.com>
2022-01-11 18:12:55 +0200

    clShaders: dao-vote WIP(10)

---
+++ bvm/Shaders/dao-vote/app-admin.cpp
@@ -0,0 +1,209 @@
+#include "../common.h"
+#include "../app_common_impl.h"
+#include "contract.h"
+#include "../upgradable2/contract.h"
+#include "../upgradable2/app_common_impl.h"
+
+#define DaoVote_manager_deploy_version(macro)
+#define DaoVote_manager_view(macro)
+#define DaoVote_manager_my_admin_key(macro)
+#define DaoVote_manager_schedule_upgrade(macro) Upgradable2_schedule_upgrade(macro)
+#define DaoVote_manager_explicit_upgrade(macro) macro(ContractID, cid)
+#define DaoVote_manager_replace_admin(macro) Upgradable2_replace_admin(macro)
+#define DaoVote_manager_set_min_approvers(macro) Upgradable2_set_min_approvers(macro)
+
+#define DaoVote_manager_deploy_contract(macro) \
+    Upgradable2_deploy(macro) \
+    macro(AssetID, aidVote) \
+    macro(Height, hEpochDuration)
+
+#define DaoVoteRole_manager(macro) \
+    macro(manager, deploy_version) \
+    macro(manager, view) \
+    macro(manager, deploy_contract) \
+    macro(manager, schedule_upgrade) \
+    macro(manager, explicit_upgrade) \
+    macro(manager, replace_admin) \
+    macro(manager, set_min_approvers) \
+    macro(manager, my_admin_key) \
+
+#define DaoVoteRoles_All(macro) \
+    macro(manager)
+
+BEAM_EXPORT void Method_0()
+{
+    // scheme
+    Env::DocGroup root("");
+
+    {   Env::DocGroup gr("roles");
+
+#define THE_FIELD(type, name) Env::DocAddText(#name, #type);
+#define THE_METHOD(role, name) { Env::DocGroup grMethod(#name);  DaoVote_##role##_##name(THE_FIELD) }
+#define THE_ROLE(name) { Env::DocGroup grRole(#name); DaoVoteRole_##name(THE_METHOD) }
+        
+        DaoVoteRoles_All(THE_ROLE)
+#undef THE_ROLE
+#undef THE_METHOD
+#undef THE_FIELD
+    }
+}
+
+#define THE_FIELD(type, name) const type& name,
+#define ON_METHOD(role, name) void On_##role##_##name(DaoVote_##role##_##name(THE_FIELD) int unused = 0)
+
+void OnError(const char* sz)
+{
+    Env::DocAddText("error", sz);
+}
+
+const char g_szAdminSeed[] = "upgr2-dao-vote";
+
+struct AdminKeyID :public Env::KeyID {
+    AdminKeyID() :Env::KeyID(&g_szAdminSeed, sizeof(g_szAdminSeed)) {}
+};
+
+ON_METHOD(manager, view)
+{
+    static const ShaderID s_pSid[] = {
+        DaoVote::s_SID,
+    };
+
+    ContractID pVerCid[_countof(s_pSid)];
+    Height pVerDeploy[_countof(s_pSid)];
+
+    ManagerUpgadable2::Walker wlk;
+    wlk.m_VerInfo.m_Count = _countof(s_pSid);
+    wlk.m_VerInfo.s_pSid = s_pSid;
+    wlk.m_VerInfo.m_pCid = pVerCid;
+    wlk.m_VerInfo.m_pHeight = pVerDeploy;
+
+    AdminKeyID kid;
+    wlk.ViewAll(&kid);
+}
+
+ON_METHOD(manager, deploy_version)
+{
+    Env::GenerateKernel(nullptr, 0, nullptr, 0, nullptr, 0, nullptr, 0, "Deploy DaoVote bytecode", 0);
+}
+
+static const Amount g_DepositCA = 3000 * g_Beam2Groth; // 3K beams
+
+ON_METHOD(manager, deploy_contract)
+{
+    AdminKeyID kid;
+    PubKey pk;
+    kid.get_Pk(pk);
+
+#pragma pack (push, 1)
+
+    struct Args
+        :public Upgradable2::Create
+        ,public DaoVote::Method::Create
+    {
+    };
+#pragma pack (pop)
+
+    Args args;
+    if (!ManagerUpgadable2::FillDeployArgs(args, &pk))
+        return;
+
+    args.m_Cfg.m_Aid = aidVote;
+    args.m_Cfg.m_hEpochDuration = hEpochDuration;
+    _POD_(args.m_Cfg.m_pkAdmin) = pk;
+
+    Env::GenerateKernel(nullptr, 0, &args, sizeof(args), nullptr, 0, nullptr, 0, "Deploy DaoVote contract", 0);
+}
+
+ON_METHOD(manager, schedule_upgrade)
+{
+    AdminKeyID kid;
+    ManagerUpgadable2::MultiSigRitual::Perform_ScheduleUpgrade(cid, kid, cidVersion, hTarget);
+}
+
+ON_METHOD(manager, explicit_upgrade)
+{
+    ManagerUpgadable2::MultiSigRitual::Perform_ExplicitUpgrade(cid);
+}
+
+ON_METHOD(manager, replace_admin)
+{
+    AdminKeyID kid;
+    ManagerUpgadable2::MultiSigRitual::Perform_ReplaceAdmin(cid, kid, iAdmin, pk);
+}
+
+ON_METHOD(manager, set_min_approvers)
+{
+    AdminKeyID kid;
+    ManagerUpgadable2::MultiSigRitual::Perform_SetApprovers(cid, kid, newVal);
+}
+
+Amount get_ContractLocked(AssetID aid, const ContractID& cid)
+{
+    Env::Key_T<AssetID> key;
+    _POD_(key.m_Prefix.m_Cid) = cid;
+    key.m_Prefix.m_Tag = KeyTag::LockedAmount;
+    key.m_KeyInContract = Utils::FromBE(aid);
+
+    struct AmountBig {
+        Amount m_Hi;
+        Amount m_Lo;
+    };
+
+    AmountBig val;
+    if (!Env::VarReader::Read_T(key, val))
+        return 0;
+
+    return Utils::FromBE(val.m_Lo);
+}
+
+ON_METHOD(manager, my_admin_key)
+{
+    PubKey pk;
+    AdminKeyID kid;
+    kid.get_Pk(pk);
+    Env::DocAddBlob_T("admin_key", pk);
+}
+
+#undef ON_METHOD
+#undef THE_FIELD
+
+BEAM_EXPORT void Method_1() 
+{
+    Env::DocGroup root("");
+
+    char szRole[0x20], szAction[0x20];
+
+    if (!Env::DocGetText("role", szRole, sizeof(szRole)))
+        return OnError("Role not specified");
+
+    if (!Env::DocGetText("action", szAction, sizeof(szAction)))
+        return OnError("Action not specified");
+
+    const char* szErr = nullptr;
+
+#define PAR_READ(type, name) type arg_##name; Env::DocGet(#name, arg_##name);
+#define PAR_PASS(type, name) arg_##name,
+
+#define THE_METHOD(role, name) \
+        if (!Env::Strcmp(szAction, #name)) { \
+            DaoVote_##role##_##name(PAR_READ) \
+            On_##role##_##name(DaoVote_##role##_##name(PAR_PASS) 0); \
+            return; \
+        }
+
+#define THE_ROLE(name) \
+    if (!Env::Strcmp(szRole, #name)) { \
+        DaoVoteRole_##name(THE_METHOD) \
+        return OnError("invalid Action"); \
+    }
+
+    DaoVoteRoles_All(THE_ROLE)
+
+#undef THE_ROLE
+#undef THE_METHOD
+#undef PAR_PASS
+#undef PAR_READ
+
+    OnError("unknown Role");
+}
+
