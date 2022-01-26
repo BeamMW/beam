81734530208ae2354f7798c7da4394610b8717dd
anatol <anatol@beam-mw.com>
2022-01-26 18:29:18 +0300

    #1841 call app shader only after the wallet has synchronized with node

--- wallet/cli/cli.cpp
+++ wallet/cli/cli.cpp
@@ -2334,7 +2334,16 @@ namespace
 
                 std::cout << "Executing shader..." << std::endl;
 
-                man.StartRun(man.m_Args.empty() ? 0 : 1); // scheme if no args
+                auto startedEvent = io::AsyncEvent::create(io::Reactor::get_Current(),
+                    [&man]()
+                    {
+                        man.StartRun(man.m_Args.empty() ? 0 : 1); // scheme if no args
+                    });
+
+                wallet->DoInSyncedWallet([startedEvent]()
+                    {
+                        startedEvent->post();
+                    });
 
                 if (!man.m_Done)
                 {
