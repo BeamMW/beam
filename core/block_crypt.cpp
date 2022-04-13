ff42cff4139be982713998580ea3db8805c7fc8e
valdok <valdo@beam-mw.com>
2022-04-13 13:24:16 +0300

    HF4 height increased

--- core/block_crypt.cpp
+++ core/block_crypt.cpp
@@ -1886,7 +1886,7 @@ namespace beam
 		pForks[1].m_Height = 30;
 		pForks[2].m_Height = 30;
 		pForks[3].m_Height = 1500;
-		pForks[4].m_Height = 515252;
+		pForks[4].m_Height = 516700;
 
 		// future forks
 		for (size_t i = 5; i < _countof(pForks); i++)
