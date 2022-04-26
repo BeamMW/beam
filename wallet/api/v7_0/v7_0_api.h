7dda5a1ec55edc4bf2ecfe50cd0c4a6bbd0dc066
chapati <chapati.th@gmail.com>
2022-04-26 16:13:31 +0300

    6.3 API -> 7.0 API

---
+++ wallet/api/v7_0/v7_0_api.h
@@ -0,0 +1,31 @@
+// Copyright 2018 The Beam Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+#include "v7_0_api_defs.h"
+#include "wallet/api/v6_1/v6_1_api.h"
+
+namespace beam::wallet
+{
+    class V70Api: public V61Api
+    {
+    public:
+        // CTOR MUST BE SAFE TO CALL FROM ANY THREAD
+        V70Api(IWalletApiHandler& handler, unsigned long avMajor, unsigned long avMinor, const ApiInitData& init);
+        ~V70Api() override = default;
+
+        V7_0_API_METHODS(BEAM_API_PARSE_FUNC)
+        V7_0_API_METHODS(BEAM_API_RESPONSE_FUNC)
+        V7_0_API_METHODS(BEAM_API_HANDLE_FUNC)
+    };
+}
