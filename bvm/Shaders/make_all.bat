5bd9a52d34f6ff631b19d55e7dd078fe4b47d2a1
valdok <valdo@beam-mw.com>
2022-06-15 11:44:09 +0300

    Shaders: make_all.bat updated

--- bvm/Shaders/make_all.bat
+++ bvm/Shaders/make_all.bat
@@ -14,6 +14,8 @@ call make_shader dao-core-testnet/app-admin
 call make_shader dao-core-testnet/contract
 call make_shader dao-vote/app
 call make_shader dao-vote/contract
+call make_shader dao-vault/app
+call make_shader dao-vault/contract
 call make_shader dummy/app
 call make_shader dummy/contract
 call make_shader Explorer/Parser
