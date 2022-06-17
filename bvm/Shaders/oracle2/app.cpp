3ff9dc6121f2d4de364df6b5eef20ef1796554cc
valdok <valdo@beam-mw.com>
2022-06-17 13:18:05 +0300

    Shaders: oracle2/app updated

--- bvm/Shaders/oracle2/app.cpp
+++ bvm/Shaders/oracle2/app.cpp
@@ -1,16 +1,44 @@
 #include "../common.h"
 #include "../app_common_impl.h"
 #include "../app_comm.h"
+#include "../upgradable3/app_common_impl.h"
 #include "contract.h"
 
-#define Oracle2_manager_create(macro) macro(uint64_t, initialVal_n)
+#define Oracle2_settings(macro) \
+    macro(Height, hValidity) \
+    macro(uint32_t, nMinProviders)
+
+#define Oracle2_manager_deploy(macro) \
+    Upgradable3_deploy(macro) \
+    Oracle2_settings(macro)
+
 #define Oracle2_manager_view(macro)
+#define Oracle2_manager_upd_settings(macro) macro(ContractID, cid) Oracle2_settings(macro)
 #define Oracle2_manager_view_params(macro) macro(ContractID, cid)
 #define Oracle2_manager_view_median(macro) macro(ContractID, cid)
+#define Oracle2_manager_schedule_upgrade(macro) Upgradable3_schedule_upgrade(macro)
+#define Oracle2_manager_replace_admin(macro) Upgradable3_replace_admin(macro)
+#define Oracle2_manager_set_min_approvers(macro) Upgradable3_set_min_approvers(macro)
+#define Oracle2_manager_explicit_upgrade(macro) macro(ContractID, cid)
+
+#define Oracle2_manager_prov_add(macro) \
+    macro(ContractID, cid) \
+    macro(PubKey, pk)
+
+#define Oracle2_manager_prov_del(macro) \
+    macro(ContractID, cid) \
+    macro(uint32_t, iProv)
 
 #define Oracle2Role_manager(macro) \
-    macro(manager, create) \
+    macro(manager, deploy) \
     macro(manager, view) \
+    macro(manager, schedule_upgrade) \
+    macro(manager, explicit_upgrade) \
+    macro(manager, replace_admin) \
+    macro(manager, set_min_approvers) \
+    macro(manager, upd_settings) \
+    macro(manager, prov_add) \
+    macro(manager, prov_del) \
     macro(manager, view_params) \
     macro(manager, view_median)
 
@@ -69,46 +97,68 @@ struct ProviderKeyID
     ProviderKeyID() :Env::KeyID(g_szKeyMaterial, sizeof(g_szKeyMaterial)) {}
 };
 
+const char g_szAdminSeed[] = "upgr3-oracle2";
+
+struct AdminKeyID :public Env::KeyID {
+    AdminKeyID() :Env::KeyID(g_szAdminSeed, sizeof(g_szAdminSeed)) {}
+};
+
+const Upgradable3::Manager::VerInfo g_VerInfo = { s_pSID, _countof(s_pSID) };
+
 ON_METHOD(manager, view)
 {
-    EnumAndDumpContracts(Oracle2::s_SID);
+    AdminKeyID kid;
+    g_VerInfo.DumpAll(&kid);
 }
 
-ON_METHOD(manager, create)
+ON_METHOD(manager, deploy)
 {
-    if (!initialVal_n)
-        return OnError("val can't be zero");
+    AdminKeyID kid;
+    PubKey pk;
+    kid.get_Pk(pk);
 
-    ProviderKeyID kid;
+    Oracle2::Method::Create arg;
 
-    Method::Create<StateFull::s_ProvsMax> arg;
-    arg.m_InitialValue = MultiPrecision::Float(initialVal_n) / get_Norm_n();
+    if (!g_VerInfo.FillDeployArgs(arg.m_Upgradable, &pk))
+        return;
 
-    arg.m_Providers = 1;
-    kid.get_Pk(arg.m_pPk[0]);
-
-    static const char s_szPrefix[] = "prov_";
-    char szBuf[_countof(s_szPrefix) + Utils::String::Decimal::Digits<StateFull::s_ProvsMax>::N];
-    Env::Memcpy(szBuf, s_szPrefix, sizeof(s_szPrefix) - sizeof(char));
-
-    for (; arg.m_Providers < StateFull::s_ProvsMax; arg.m_Providers++)
-    {
-        Utils::String::Decimal::Print(szBuf + _countof(s_szPrefix) - 1, arg.m_Providers);
-        if (!Env::DocGetBlob(szBuf, arg.m_pPk + arg.m_Providers, sizeof(arg.m_pPk[arg.m_Providers])))
-            break;
-    }
+    arg.m_Settings.m_hValidity = hValidity;
+    arg.m_Settings.m_MinProviders = nMinProviders;
 
     const uint32_t nCharge =
-        Env::Cost::CallFar +
+        Upgradable3::Manager::get_ChargeDeploy() +
+        Env::Cost::SaveVar_For(sizeof(State0)) +
         Env::Cost::SaveVar_For(sizeof(Median)) +
-        Env::Cost::SaveVar_For(sizeof(StateFull)) +
-        Env::Cost::Cycle * (200 + 200 * StateFull::s_ProvsMax);
+        Env::Cost::Cycle * 300;
 
-    Env::GenerateKernel(nullptr, arg.s_iMethod, &arg, sizeof(Method::Create<0>) + sizeof(arg.m_pPk[0]) * arg.m_Providers, nullptr, 0, nullptr, 0, "create Oracle2 contract", nCharge);
+    Env::GenerateKernel(nullptr, 0, &arg, sizeof(arg), nullptr, 0, nullptr, 0, "Deploy Oracle2 contract", nCharge);
+}
+
+ON_METHOD(manager, schedule_upgrade)
+{
+    AdminKeyID kid;
+    g_VerInfo.ScheduleUpgrade(cid, kid, hTarget);
+}
+
+ON_METHOD(manager, explicit_upgrade)
+{
+    Upgradable3::Manager::MultiSigRitual::Perform_ExplicitUpgrade(cid);
+}
+
+ON_METHOD(manager, replace_admin)
+{
+    AdminKeyID kid;
+    Upgradable3::Manager::MultiSigRitual::Perform_ReplaceAdmin(cid, kid, iAdmin, pk);
+}
+
+ON_METHOD(manager, set_min_approvers)
+{
+    AdminKeyID kid;
+    Upgradable3::Manager::MultiSigRitual::Perform_SetApprovers(cid, kid, newVal);
 }
 
 struct MyState
-    :public StateFull
+    :public StateMax
 {
     uint32_t m_Provs;
     uint32_t m_iProv;
@@ -125,12 +175,12 @@ struct MyState
 
         Env::VarReader r(k, k);
 
-        uint32_t nKey = sizeof(k), nVal = sizeof(StateFull);
+        uint32_t nKey = sizeof(k), nVal = sizeof(StateMax);
         if (!r.MoveNext(&k, nKey, this, nVal, KeyTag::Internal))
             return false;
 
-        m_Provs = nVal / sizeof(StateFull::Entry);
-        if ((m_Provs > s_ProvsMax) || !m_Provs)
+        m_Provs = (nVal - sizeof(State0)) / sizeof(Entry);
+        if (m_Provs > s_ProvsMax)
             return false;
 
         ProviderKeyID kid;
@@ -152,7 +202,84 @@ struct MyState
         OnError("state not found");
         return false;
     }
+
+    uint32_t get_ChargeUpd_NoCall() const
+    {
+        uint32_t nSize = sizeof(State0) + sizeof(Entry) * m_Provs;
+
+        return
+            Env::Cost::CallFar +
+            Env::Cost::LoadVar_For(nSize + sizeof(Entry)) +
+            Env::Cost::SaveVar_For(nSize) +
+            Env::Cost::SaveVar_For(sizeof(Median)) +
+            Env::Cost::Cycle * (1000 + 1000 * m_Provs);
+    }
+
+    uint32_t get_ChargeUpd() const
+    {
+        return
+            Env::Cost::CallFar +
+            get_ChargeUpd_NoCall();
+    }
 };
+
+ON_METHOD(manager, upd_settings)
+{
+    MyState s;
+    if (!s.Read(cid))
+        return;
+
+    Oracle2::Method::SetSettings arg;
+    arg.m_Settings.m_hValidity = hValidity;
+    arg.m_Settings.m_MinProviders = nMinProviders;
+
+    Upgradable3::Manager::MultiSigRitual msp;
+    msp.m_szComment = "Oracle2 update settings";
+    msp.m_iMethod = arg.s_iMethod;
+    msp.m_nArg = sizeof(arg);
+    msp.m_pCid = &cid;
+    msp.m_Kid = AdminKeyID();
+    msp.m_Charge += s.get_ChargeUpd_NoCall();
+    msp.Perform(arg);
+}
+
+ON_METHOD(manager, prov_add)
+{
+    MyState s;
+    if (!s.Read(cid))
+        return;
+
+    Oracle2::Method::ProviderAdd arg;
+    _POD_(arg.m_pk) = pk;
+
+    Upgradable3::Manager::MultiSigRitual msp;
+    msp.m_szComment = "Oracle2 provider add";
+    msp.m_iMethod = arg.s_iMethod;
+    msp.m_nArg = sizeof(arg);
+    msp.m_pCid = &cid;
+    msp.m_Kid = AdminKeyID();
+    msp.m_Charge += s.get_ChargeUpd_NoCall();
+    msp.Perform(arg);
+}
+
+ON_METHOD(manager, prov_del)
+{
+    MyState s;
+    if (!s.Read(cid))
+        return;
+
+    Oracle2::Method::ProviderDel arg;
+    arg.m_iProvider = iProv;
+
+    Upgradable3::Manager::MultiSigRitual msp;
+    msp.m_szComment = "Oracle2 provider del";
+    msp.m_iMethod = arg.s_iMethod;
+    msp.m_nArg = sizeof(arg);
+    msp.m_pCid = &cid;
+    msp.m_Kid = AdminKeyID();
+    msp.m_Charge += s.get_ChargeUpd_NoCall();
+    msp.Perform(arg);
+}
 
 ON_METHOD(manager, view_params)
 {
@@ -172,9 +299,8 @@ ON_METHOD(manager, view_params)
 
         const auto& x = s.m_pE[iProv];
         Env::DocAddBlob_T("pk", x.m_Pk);
-
-        auto iPos = x.m_iPos;
-        Env::DocAddNum("val", s.m_pE[iPos].m_Val * get_Norm_n());
+        Env::DocAddNum("val", x.m_Val * get_Norm_n());
+        Env::DocAddNum("hUpd", x.m_hUpdated);
     }
 }
 
@@ -184,11 +310,12 @@ ON_METHOD(manager, view_median)
     _POD_(k.m_Prefix.m_Cid) = cid;
     k.m_KeyInContract = Tags::s_Median;
 
-    ValueType val;
-    if (Env::VarReader::Read_T(k, val))
+    Median med;
+    if (Env::VarReader::Read_T(k, med))
     {
         Env::DocArray gr("res");
-        Env::DocAddNum("val", val * get_Norm_n());
+        Env::DocAddNum("val", med.m_Res * get_Norm_n());
+        Env::DocAddNum("hEnd", med.m_hEnd);
     }
     else
         OnError("not found");
@@ -217,17 +344,11 @@ ON_METHOD(provider, set)
     if (!s.IsProvider())
         return OnError("not a provider");
 
-    Method::Set arg;
+    Method::FeedData arg;
     arg.m_iProvider = s.m_iProv;
     arg.m_Value = MultiPrecision::Float(val_n) / get_Norm_n();
 
-    const uint32_t nCharge =
-        Env::Cost::CallFar +
-        Env::Cost::LoadVar_For(sizeof(StateFull)) +
-        Env::Cost::SaveVar_For(sizeof(StateFull)) +
-        Env::Cost::AddSig +
-        Env::Cost::SaveVar_For(sizeof(Median)) +
-        Env::Cost::Cycle * (1500 + 500 * StateFull::s_ProvsMax);
+    const uint32_t nCharge = s.get_ChargeUpd();
 
     ProviderKeyID kid;
     Env::GenerateKernel(&cid, arg.s_iMethod, &arg, sizeof(arg), nullptr, 0, &kid, 1, "Oracle2 set val", nCharge);
