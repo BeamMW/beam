7612166a46f2da90defa17e3bf022db08e1d4284
valdok <valdo@beam-mw.com>
2023-02-16 10:16:13 +0200

    wallet: ContractTransaction lifetime reduced to 5 blocks

--- wallet/core/contract_transaction.cpp
+++ wallet/core/contract_transaction.cpp
@@ -275,7 +275,7 @@ namespace beam::wallet
                 Block::SystemState::Full sTip;
                 if (GetTip(sTip))
                 {
-                    builder.m_Height.m_Max = sTip.m_Height + 20; // 20 blocks - standard contract tx life time
+                    builder.m_Height.m_Max = sTip.m_Height + 5; // 5 blocks - standard contract tx life time
                     SetParameter(TxParameterID::MaxHeight, builder.m_Height.m_Max, GetSubTxID());
                 }
             }
