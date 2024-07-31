538fe6188a2e5f9806bed3e343a66d32ef2c14c4
valdok <valdo@beam-mw.com>
2024-07-31 15:57:42 +0300

    added NumericUtils

---
+++ core/numeric_utils.cpp
@@ -0,0 +1,462 @@
+// Copyright 2018 The Beam Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "numeric_utils.h"
+
+namespace beam {
+
+namespace NumericUtils {
+
+	namespace details
+	{
+		template <uint32_t nBits>
+		struct PerBits {
+
+			template <typename T>
+			static uint32_t clz(T x)
+			{
+				const uint32_t nHalf = nBits / 2;
+				if constexpr (nHalf > 0)
+				{
+					auto x1 = static_cast<uint32_t>(x >> nHalf);
+					if (x1)
+						return PerBits<nHalf>::clz(x1);
+
+					return nHalf + PerBits<nHalf>::clz(static_cast<uint32_t>(x));
+				}
+				else
+				{
+					assert(x <= 1);
+					return !x;
+				}
+			}
+		};
+	}
+
+	template <typename T>
+	uint32_t clz(T x)
+	{
+		static_assert(std::is_integral_v<T>, "");
+		static_assert(!std::is_signed_v<T>, "");
+		return details::PerBits<sizeof(T) * 8>::clz(x);
+	}
+
+	template uint32_t clz<uint64_t>(uint64_t);
+	template uint32_t clz<uint32_t>(uint32_t);
+	template uint32_t clz<uint16_t>(uint16_t);
+	template uint32_t clz<uint8_t>(uint8_t);
+
+} // namespace NumericUtils
+
+
+namespace MultiWord {
+
+void Slice::Copy(ConstSlice x) const
+{
+	// should not overlap
+	if (x.m_n >= m_n)
+		CopyInternal(x.get_TailPtr(m_n));
+	else
+	{
+		auto h = *this;
+		auto t = Cast::Up<Slice>(h.CutTail(x.m_n));
+		h.Set0();
+		t.CopyInternal(x.m_p);
+	}
+}
+
+void Slice::CopyInternal(const Word* p) const
+{
+	for (uint32_t i = 0; i < m_n; i++)
+		m_p[i] = p[i];
+}
+
+void Slice::LShiftRaw(Word* pDst, const Word* pSrc, uint32_t nLen, uint32_t nShiftL, Word& w)
+{
+	assert(nShiftL && (nShiftL < nWordBits));
+	uint32_t nShiftR = nWordBits - nShiftL;
+
+	while (nLen--)
+	{
+		Word w1 = pSrc[nLen];
+		pDst[nLen] = (w1 << nShiftL) | w;
+		w = w1 >> nShiftR;
+	}
+}
+
+void Slice::RShiftRaw(Word* pDst, const Word* pSrc, uint32_t nLen, uint32_t nShiftR, Word& w)
+{
+	assert(nShiftR && (nShiftR < nWordBits));
+	uint32_t nShiftL = nWordBits - nShiftR;
+
+	for (uint32_t i = 0; i < nLen; i++)
+	{
+		Word w1 = pSrc[i];
+		pDst[i] = w | (w1 >> nShiftR);
+		w = w1 << nShiftL;
+	}
+}
+
+void Slice::LShiftNormalized(uint32_t nShiftL, ConstSlice src) const
+{
+	Word w = 0;
+
+	if (m_n <= src.m_n)
+		LShiftRaw(m_p, src.get_TailPtr(m_n), m_n, nShiftL, w);
+	else
+	{
+		auto x = *this;
+		LShiftRaw(x.CutTail(src.m_n).m_p, src.m_p, src.m_n, nShiftL, w);
+
+		assert(x.m_n);
+		x.m_p[--x.m_n] = w;
+
+		x.Set0();
+	}
+}
+
+void Slice::RShiftNormalized(uint32_t nShiftR, ConstSlice src) const
+{
+	Word w = 0;
+
+	if (m_n <= src.m_n)
+	{
+		if (m_n < src.m_n)
+			w = src.m_p[src.m_n - m_n - 1] << (nWordBits - nShiftR);
+		RShiftRaw(m_p, src.get_TailPtr(m_n), m_n, nShiftR, w);
+	}
+	else
+	{
+		auto x = *this;
+		RShiftRaw(x.CutTail(src.m_n).m_p, src.m_p, src.m_n, nShiftR, w);
+		x.Set0();
+	}
+}
+
+void Slice::LShift(ConstSlice a, uint32_t nBits)
+{
+	uint32_t nWords = nBits / nWordBits;
+	if (nWords)
+	{
+		nBits %= nWordBits;
+
+		if (nWords >= m_n)
+			Set0();
+		else
+		{
+			auto x = *this;
+			Cast::Up<Slice>(x.CutTail(nWords)).Set0();
+			x.LShiftNormalized(nBits, a);
+		}
+	}
+	else
+		LShiftNormalized(nBits, a);
+}
+
+void Slice::RShift(ConstSlice a, uint32_t nBits)
+{
+	uint32_t nWords = nBits / nWordBits;
+	if (nWords)
+	{
+		nBits %= nWordBits;
+
+		if (nWords >= a.m_n)
+			Set0();
+		else
+		{
+			a.m_n -= nWords;
+			RShiftNormalized(nBits, a);
+		}
+
+	}
+	else
+		RShiftNormalized(nBits, a);
+}
+
+template <bool bAdd>
+void Slice::AddOrSub(ConstSlice b, DWord& carry) const
+{
+	if (b.m_n >= m_n)
+		AddOrSub_Internal<bAdd, true>(b.get_TailPtr(m_n), carry);
+	else
+	{
+		auto x = *this;
+
+		Cast::Up<Slice>(x.CutTail(b.m_n)).AddOrSub_Internal<bAdd, true>(b.m_p, carry);
+		x.AddOrSub_Internal<bAdd, false>(nullptr, carry);
+	}
+}
+
+template void Slice::AddOrSub<true>(ConstSlice b, DWord& carry) const;
+template void Slice::AddOrSub<false>(ConstSlice b, DWord& carry) const;
+
+template <bool bAdd>
+void Slice::AddOrSub_Mul(ConstSlice a, ConstSlice b) const
+{
+	if (!m_n)
+		return;
+
+	a.Trim();
+	b.Trim();
+
+	if (a.m_n < b.m_n)
+		std::swap(a, b);
+
+	if (!b.m_n)
+		return;
+
+	if (m_n >= a.m_n + b.m_n)
+		AddOrSubMul_Internal<bAdd, true>(a, b);
+	else
+	{
+		auto x = *this;
+
+		if (a.m_n > m_n)
+		{
+			a = a.get_Tail(m_n);
+
+			if (b.m_n > m_n)
+				b = b.get_Tail(m_n);
+		}
+		else
+		{
+			if (m_n > a.m_n)
+			{
+				AddOrSubMul_Internal<bAdd, true>(a, b.CutTail(m_n - a.m_n));
+				x.m_n = a.m_n;
+			}
+		}
+
+		x.AddOrSubMul_Internal<bAdd, false>(a, b);
+	}
+}
+
+template void Slice::AddOrSub_Mul<true>(ConstSlice a, ConstSlice b) const;
+template void Slice::AddOrSub_Mul<false>(ConstSlice a, ConstSlice b) const;
+
+template <bool bAdd, bool bOp2>
+void Slice::AddOrSub_Internal(const Word* b, DWord& carry) const
+{
+	for (uint32_t i = m_n; i; )
+	{
+		if constexpr (!bOp2)
+		{
+			if (!carry)
+				break;
+		}
+
+		i--;
+
+		if constexpr (bOp2)
+		{
+			if constexpr (bAdd)
+				carry += b[i];
+			else
+				carry -= b[i];
+		}
+
+		AddToWord(m_p[i], carry);
+		MoveCarry<bAdd>(carry);
+	}
+}
+
+template <bool bAdd>
+void Slice::AddOrSub_Mul_Once(ConstSlice a, Word mul, DWord& carry) const
+{
+	assert(m_n >= a.m_n);
+	auto x = *this;
+	auto y = x.CutTail(a.m_n);
+
+	for (uint32_t i = a.m_n; i--; )
+	{
+		auto val = static_cast<DWord>(a.m_p[i]) * mul;
+		if constexpr (bAdd)
+			carry += val;
+		else
+			carry -= val;
+
+		AddToWord(y.m_p[i], carry);
+
+		if constexpr (bAdd)
+			MoveCarry<true>(carry);
+		else
+		{
+			// do it carefully. Carry may be negative, even though msb may not be set
+			// consider it negative if hiword is nnz
+			MoveCarry<true>(carry);
+			if (carry)
+				carry |= static_cast<DWord>(static_cast<Word>(-1)) << nWordBits;
+		}
+	}
+
+	x.AddOrSub_Internal<bAdd, false>(nullptr, carry);
+}
+
+template <bool bAdd, bool bFit>
+void Slice::AddOrSubMul_Internal(ConstSlice a, ConstSlice b) const
+{
+	assert(a.m_n >= b.m_n);
+	assert(b.m_n);
+
+	Slice x = *this;
+	if constexpr (bFit)
+		assert(m_n >= a.m_n + b.m_n);
+	else
+		assert(m_n == a.m_n);
+
+	while (true)
+	{
+		DWord carry = 0;
+		Word mul = b.m_p[--b.m_n];
+
+		x.AddOrSub_Mul_Once<bAdd>(a, mul, carry);
+
+		if (!b.m_n)
+			break;
+
+		x.m_n--;
+
+		if constexpr (!bFit)
+		{
+			a.m_p++;
+			a.m_n--;
+		}
+	}
+}
+
+void Slice::SetDivResid(Slice resid, ConstSlice div, Word* pBufResid, Word* pBufDiv) const
+{
+	// me = resid / div
+	// resid -= me * div (residual)
+	//
+	// the result is truncated is this obj is too short, yet resid will be correct
+
+	div.Trim();
+	if (div.m_n)
+	{
+		uint32_t nz = NumericUtils::clz(div.m_p[0]);
+		assert(nz < nWordBits);
+
+		if (nz)
+		{
+			Slice div2;
+			div2.m_p = pBufDiv;
+			div2.m_n = div.m_n;
+			div2.LShiftNormalized(nz, div);
+
+
+			Slice resid2;
+			resid2.m_p = pBufResid;
+			resid2.m_n = resid.m_n + 1;
+			resid2.LShiftNormalized(nz, resid.get_Const());
+
+			SetDivResidNormalized(resid2, div2.get_Const());
+
+			resid.RShiftNormalized(nz, resid2.get_Const());
+		}
+		else
+			SetDivResidNormalized(resid, div);
+	}
+	else
+		SetMax();
+}
+
+void Slice::SetDivResidNormalized(Slice resid, ConstSlice div) const
+{
+	// divisor is normalized, i.e. msb is set
+	if (resid.m_n < div.m_n)
+		Set0();
+	else
+	{
+		uint32_t nResMax = resid.m_n - div.m_n + 1;
+
+		if (m_n > nResMax)
+			Cast::Up<Slice>(get_Head(m_n - nResMax)).Set0();
+
+		Word hiDenom = div.m_p[0];
+		Word hiNom = 0;
+		DWord nom = resid.m_p[0];
+
+		resid.m_n = div.m_n;
+
+		for (uint32_t i = 0; ; )
+		{
+			// init-guess
+			Word res = static_cast<Word>(nom / hiDenom);
+			//
+			// the actual result is within: Nom/(hiDenom+1) < guess < (Nom+1)/hiDenom
+			//
+			// since denom is normalized (msb is set), the result is actually limited within: (guess-2) <= res <= guess
+
+			if (!res)
+			{
+				// could be overflow
+				if (hiNom)
+				{
+					assert(hiNom == hiDenom);
+					res = static_cast<Word>(-1);
+				}
+			}
+
+			if (res)
+			{
+				// multiply & subtract
+				DWord carry = 0;
+				resid.AddOrSub_Mul_Once<false>(div, res, carry);
+
+				if (carry)
+				{
+					// overflow
+					res--;
+					carry = 0;
+					resid.AddOrSub<true>(div, carry);
+
+					if (!carry)
+					{
+						// overflow (back) wasn't reached. Do it again
+						res--;
+						carry = 0;
+						resid.AddOrSub<true>(div, carry);
+
+						assert(carry);
+					}
+				}
+
+			}
+
+			// store result
+			if (m_n + i >= nResMax)
+				m_p[m_n + i - nResMax] = res;
+
+			// advance
+			if (i)
+			{
+				assert(!resid.m_p[0]);
+				resid.m_p++;
+			}
+			else
+				resid.m_n++;
+
+			if (++i == nResMax)
+				break;
+
+			hiNom = resid.m_p[0];
+			nom = (static_cast<DWord>(hiNom) << nWordBits) | resid.m_p[1];
+		}
+
+	}
+}
+
+} // namespace MultiWord
+} // namespace beam
---
+++ core/numeric_utils.h
@@ -0,0 +1,503 @@
+// Copyright 2018 The Beam Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include "common.h"
+
+#ifdef _MSC_VER
+#   define __restrict__ __restrict
+#endif // 
+
+namespace beam {
+
+namespace NumericUtils
+{
+	template <typename T>
+	uint32_t clz(T x); // count leading zeroes
+
+	namespace details
+	{
+		template <uint32_t n>
+		struct Power
+		{
+			template <uint64_t x>
+			struct Of
+			{
+				static const uint64_t N = Power<n / 2>::template Of<x>::N * Power<n - (n/2)>::template Of<x>::N;
+			};
+
+			template <typename T, T base>
+			static uint32_t Log(T& res, T x)
+			{
+				assert(x < Of<base>::N);
+
+				const uint32_t nHalf = (n + 1) / 2; // round to bigger side
+				const T vRoot = Power<nHalf>::template Of<base>::N;
+
+				bool bBigger = (x >= vRoot);
+				if (bBigger)
+					x /= vRoot;
+
+				uint32_t nRet = Power<nHalf>::template Log<T, base>(res, x);
+
+				if (bBigger)
+				{
+					nRet += nHalf;
+					res *= vRoot;
+				}
+
+				return nRet;
+			}
+
+			template <typename T, T base>
+			static T Raise(uint32_t n_)
+			{
+				const uint32_t nHalf = (n + 1) / 2; // round to bigger side
+
+				if (n_ < n)
+					return Power<nHalf>::template Raise<T, base>(n_);
+
+				return Of<base>::N * Power<nHalf>::template Raise<T, base>(n_ - n);
+			}
+
+		};
+
+		template <>
+		struct Power<1>
+		{
+			template <uint64_t x>
+			struct Of
+			{
+				static const uint64_t N = x;
+			};
+
+			template <typename T, T base>
+			static uint32_t Log(T& res, T x)
+			{
+				assert(x < base);
+
+				res = 1;
+				return 0;
+			}
+
+			template <typename T, T base>
+			static T Raise(uint32_t n_)
+			{
+				return n_ ? base : 1;
+			}
+
+		};
+
+		template <uint64_t n>
+		struct Log
+		{
+			template <uint64_t base>
+			struct Of
+			{
+				// rounded down
+				static const uint64_t N = 1 + Log<n / base>::template Of<base>::N;
+			};
+		};
+
+		template <>
+		struct Log<0>
+		{
+			template <uint64_t base>
+			struct Of
+			{
+				static const int32_t N = -1;
+			};
+		};
+
+	}
+
+	// Compile-time power and log
+
+	template <uint64_t x, uint32_t n>
+	struct PowerOf
+	{
+		static const uint64_t N = details::Power<n>::template Of<x>::N;
+	};
+
+	template <uint64_t x, uint32_t base>
+	struct LogOf
+	{
+		// rounded down
+		static const uint32_t N = details::Log<x>::template Of<base>::N;
+	};
+
+	template <typename T, T base>
+	T Power(uint32_t n)
+	{
+		// find highest power, for which base^n <= bound
+		const uint32_t nMax = LogOf<(T) -1, base>::N;
+		return details::Power<nMax>::template Raise<T, base>(n);
+	}
+
+	template <typename T, T base>
+	uint32_t Log(T& res, T x)
+	{
+		// find highest power, for which base^n <= bound
+		const uint32_t nMax = LogOf<(T) -1, base>::N;
+		const T vMax = PowerOf<base, nMax>::N;
+
+		if (x >= vMax)
+		{
+			res = vMax;
+			return nMax;
+		}
+
+		return details::Power<nMax>::template Log<T, base>(res, x);
+	}
+}
+
+namespace MultiWord {
+
+	typedef uint32_t Word;
+	typedef uint64_t DWord;
+	typedef int64_t DWordSigned;
+
+	static const uint32_t nWordBits = sizeof(Word) * 8;
+
+	template <typename T>
+	struct BaseSlice
+	{
+		T* m_p; // msb at the beginning
+		uint32_t m_n;
+
+		T get_Safe(uint32_t i) const
+		{
+			return (i < m_n) ? m_p[i] : 0;
+		}
+
+		BaseSlice get_Head(uint32_t n) const
+		{
+			assert(n <= m_n);
+			return BaseSlice{ m_p, n };
+		}
+
+		T* get_TailPtr(uint32_t n) const
+		{
+			assert(n <= m_n);
+			return m_p + m_n - n;
+		}
+
+		BaseSlice get_Tail(uint32_t n) const
+		{
+			return BaseSlice{ get_TailPtr(n), n };
+		}
+
+		BaseSlice CutTail(uint32_t n)
+		{
+			auto ret = get_Tail(n);
+			m_n -= n;
+			return ret;
+		}
+
+		bool IsZero() const
+		{
+			for (uint32_t i = 0; i < m_n; i++)
+				if (m_p[i])
+					return false;
+			return true;
+		}
+
+		void Trim()
+		{
+			// remove leading zeroes
+			while (m_n)
+			{
+				if (m_p[0])
+					break;
+
+				m_p++;
+				m_n--;
+			}
+		}
+
+		int cmp(BaseSlice x) const
+		{
+			auto me = *this;
+			me.Trim();
+			x.Trim();
+
+			if (me.m_n < x.m_n)
+				return -1;
+			if (me.m_n > x.m_n)
+				return 1;
+
+			for (uint32_t i = 0; i < me.m_n; i++)
+			{
+				if (me.m_p[i] < x.m_p[i])
+					return -1;
+				if (me.m_p[i] > x.m_p[i])
+					return 1;
+			}
+
+			return 0;
+		}
+	};
+
+	typedef BaseSlice<const Word> ConstSlice;
+
+	struct Slice
+		:public BaseSlice<Word>
+	{
+		// msb at the beginning
+
+		ConstSlice get_Const() const
+		{
+			return ConstSlice{ m_p, m_n };
+		}
+
+		void Fill(Word w) const
+		{
+			for (uint32_t i = 0; i < m_n; i++)
+				m_p[i] = w;
+		}
+
+		void Set0() const { Fill(0); }
+		void SetMax() const { Fill(static_cast<Word>(-1)); }
+
+		void Copy(ConstSlice) const;
+
+		template <bool bAdd>
+		void Set_AddOrSub(ConstSlice a, ConstSlice b, DWord& carry) const
+		{
+			Copy(a);
+			AddOrSub<bAdd>(b, carry);
+		}
+
+		template <bool bAdd>
+		void AddOrSub(ConstSlice b, DWord& carry) const;
+
+		void SetMul(ConstSlice a, ConstSlice b) const
+		{
+			Set0();
+			AddOrSub_Mul<true>(a, b);
+		}
+
+		template <bool bAdd>
+		void AddOrSub_Mul(ConstSlice a, ConstSlice b) const;
+
+		void SetDivResid(Slice resid, ConstSlice div, Word* pBufResid, Word* pBufDiv) const;
+
+		void LShift(ConstSlice a, uint32_t nBits);
+		void RShift(ConstSlice a, uint32_t nBits);
+
+	private:
+
+		static void LShiftRaw(Word* pDst, const Word* pSrc, uint32_t nLen, uint32_t nShiftL, Word& w);
+		static void RShiftRaw(Word* pDst, const Word* pSrc, uint32_t nLen, uint32_t nShiftR, Word& w);
+		void LShiftNormalized(uint32_t nShiftL, ConstSlice src) const;
+		void RShiftNormalized(uint32_t nShiftR, ConstSlice src) const;
+
+		void CopyInternal(const Word* p) const;
+
+		static void AddToWord(Word& w, DWord& carry)
+		{
+			carry += w;
+			w = static_cast<Word>(carry);
+		}
+
+		template <bool bPositive>
+		static void MoveCarry(DWord& carry)
+		{
+			if constexpr (bPositive)
+				carry >>= nWordBits;
+			else
+				((DWordSigned&)carry) >>= nWordBits;
+		}
+
+		template <bool bAdd, bool bOp2>
+		void AddOrSub_Internal(const Word* b, DWord& carry) const;
+
+		template <bool bAdd>
+		void AddOrSub_Mul_Once(ConstSlice a, Word mul, DWord& carry) const;
+
+		template <bool bAdd, bool bFit>
+		void AddOrSubMul_Internal(ConstSlice a, ConstSlice b) const;
+
+		void SetDivResidNormalized(Slice resid, ConstSlice div) const;
+	};
+
+	template <uint32_t nWords_>
+	struct Number
+	{
+		static const uint32_t nWords = nWords_;
+		static const uint32_t nSize = sizeof(Word) * nWords;
+
+		Word m_p[nWords];
+
+		Number()
+		{
+			static_assert(sizeof(Number) == nSize, "");
+		}
+		template <typename T>
+		Number(const T& x)
+		{
+			static_assert(sizeof(Number) == nSize, "");
+			operator = (x);
+		}
+
+		ConstSlice get_ConstSlice() const {
+			return ConstSlice{ m_p, nWords };
+		}
+
+		Slice get_Slice() {
+			return Slice{ m_p, nWords };
+		}
+
+		Number& operator = (Word x)
+		{
+			static_assert(nWords >= 1);
+			m_p[nWords - 1] = x;
+
+			if constexpr (nWords > 1)
+				get_Slice().get_Head(nWords - 1).Set0();
+
+			return *this;
+		}
+
+		Number& operator = (uint64_t x)
+		{
+			static_assert(nWords >= 1);
+			m_p[nWords - 1] = static_cast<Word>(x);
+
+			if constexpr (nWords > 1)
+			{
+				m_p[nWords - 2] = static_cast<Word>(x >> nWordBits);
+
+				if constexpr (nWords > 2)
+					get_Slice().get_Head(nWords - 2).Set0();
+			}
+
+			return *this;
+		}
+
+		template <uint32_t wa>
+		Number& operator = (const Number<wa>& a)
+		{
+			get_Slice().Copy(a.get_ConstSlice());
+			return *this;
+		}
+
+		template <uint32_t wa>
+		int cmp(const Number<wa>& a)
+		{
+			get_Slice().get_Const().cmp(a.get_Slice());
+		}
+
+		template <typename T> bool operator < (const T& x) const { return cmp(x) < 0; }
+		template <typename T> bool operator > (const T& x) const { return cmp(x) > 0; }
+		template <typename T> bool operator <= (const T& x) const { return cmp(x) <= 0; }
+		template <typename T> bool operator >= (const T& x) const { return cmp(x) >= 0; }
+		template <typename T> bool operator == (const T& x) const { return cmp(x) == 0; }
+		template <typename T> bool operator != (const T& x) const { return cmp(x) != 0; }
+
+		template <uint32_t wa>
+		Number& operator += (const Number<wa>& a)
+		{
+			DWord carry = 0;
+			get_Slice().AddOrSub<true>(a.get_ConstSlice(), carry);
+			return *this;
+		}
+
+		template <uint32_t wa>
+		Number& operator -= (const Number<wa>& a)
+		{
+			DWord carry = 0;
+			get_Slice().AddOrSub<false>(a.get_ConstSlice(), carry);
+			return *this;
+		}
+
+		template <uint32_t wa>
+		Number< 1 + ((nWords >= wa) ? nWords : wa) > operator + (const Number<wa>& a) const
+		{
+			Number< 1 + ((nWords >= wa) ? nWords : wa) > ret;
+			if constexpr (nWords >= wa)
+			{
+				ret = *this;
+				ret += a;
+			}
+			else
+			{
+				ret = a;
+				ret += *this;
+			}
+
+			return ret;
+		}
+
+		template <uint32_t wa>
+		Number< 1 + ((nWords >= wa) ? nWords : wa) > operator - (const Number<wa>& a) const
+		{
+			Number< 1 + ((nWords >= wa) ? nWords : wa) > ret;
+			ret = *this;
+			ret -= a;
+			return ret;
+		}
+
+		template <uint32_t wa>
+		Number<nWords + wa> operator * (const Number<wa>& a) const
+		{
+			Number<nWords + wa> ret;
+			ret.get_Slice().Set0();
+			ret.get_Slice().AddOrSub_Mul<true>(get_ConstSlice(), a.get_ConstSlice());
+			return ret;
+		}
+
+		template <uint32_t wa>
+		Number& operator *= (const Number<wa>& a)
+		{
+			auto x = *this; // copy
+			get_Slice().Set0();
+			get_Slice().AddOrSub_Mul<true>(x.get_ConstSlice(), a.get_ConstSlice());
+			return *this;
+		}
+
+		template <uint32_t wResid, uint32_t wDiv>
+		void SetDivResid(Number<wResid>& __restrict__ resid, const Number<wDiv>& __restrict__ div)
+		{
+			Word pBufResid[wResid + 1];
+			Word pBufDiv[wDiv];
+			get_Slice().SetDivResid(resid.get_Slice(), div.get_ConstSlice(), pBufResid, pBufDiv);
+		}
+
+		template <uint32_t wResid, uint32_t wDiv>
+		void SetDiv(Number<wResid> resid, const Number<wDiv>& __restrict__ div)
+		{
+			// resid is passed by val
+			SetDivResid(resid, div);
+		}
+
+		template <uint32_t wDiv>
+		Number<nWords> operator / (const Number<wDiv>& __restrict__ div) const
+		{
+			Number<nWords> ret;
+			ret.SetDiv(*this, div);
+			return ret;
+		}
+
+	};
+
+
+	template <typename T>
+	inline Number< (sizeof(T) + sizeof(Word) - 1) / sizeof(Word)> From(T x)
+	{
+		return Number< (sizeof(T) + sizeof(Word) - 1) / sizeof(Word)>(x);
+	}
+
+} // namespace MultiWord
+} // namespace beam
