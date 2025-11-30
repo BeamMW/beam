e42620909c4f93e90dc0963a1476a03e2fde82b1
valdok <valdo@beam-mw.com>
2025-11-30 20:46:21 +0200

    pbft WIP(12)

--- node/processor.h
+++ node/processor.h
@@ -594,6 +594,7 @@ public:
 		virtual const Block::Pbft::State::IValidatorSet& get_Validators() = 0;
 		virtual void OnContractVarChange(const Blob& key, const Blob& val, bool bTemporary) = 0;
 		virtual void OnContractStoreReset() = 0;
+		virtual bool OnContractInvoke(const ContractID&, uint32_t iMethod, const Blob& args, bool bTemporary) = 0;
 		virtual TxKernel::Ptr GeneratePbftRewardKernel(Amount fees, ECC::Scalar::Native& sk) = 0;
 	};
 
