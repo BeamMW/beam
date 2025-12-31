06edbe2180c8004a88394c8344dacf8fd9816e4b
valdok <valdo@beam-mw.com>
2025-12-31 17:12:42 +0200

    explorer: rich info for contracts deployed/invoked in a treasury block

--- explorer/explorer_node.cpp
+++ explorer/explorer_node.cpp
@@ -51,6 +51,8 @@ int main(int argc, char* argv[]) {
     beam::Rules r;
     beam::Rules::Scope scopeRules(r);
 
+    SetCurrentDirectoryW(L"C:\\Beam\\dappnet2");
+
     Options options;
     if (!parse_cmdline(argc, argv, options, r)) {
         return 1;
